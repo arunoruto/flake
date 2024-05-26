3a76eaa2a632b801e36eb92de10dbd703a834f48
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-05-26 15:51:06 +0200

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Mirzas Nix Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.zangetsu = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
