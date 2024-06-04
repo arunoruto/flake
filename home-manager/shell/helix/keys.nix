041f60327b78a8aabc4c552e260391cd4619bb5e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-06-04 17:56:21 +0900

    Move buffer options to space+b
    The b key is already taken as a shortcut to move back a word. So use
    space+b to open buffer options.

--- home-manager/shell/helix/keys.nix
+++ home-manager/shell/helix/keys.nix
@@ -2,12 +2,12 @@
   programs.helix.settings.keys = {
     normal = {
       space.W = [":toggle soft-wrap.enable"];
-      # b = {
-      #   p = "buffer_picker";
-      #   h = "goto_previous_buffer";
-      #   l = "goto_next_buffer";
-      #   x = ":buffer-close";
-      # };
+      space.b = {
+        p = "buffer_picker";
+        h = "goto_previous_buffer";
+        l = "goto_next_buffer";
+        x = ":buffer-close";
+      };
     };
   };
 }
