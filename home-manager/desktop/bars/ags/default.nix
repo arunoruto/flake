e2ea04f1f16c3a8290606a038db7ca3e10fb26bf
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-06-23 23:58:49 +0200

    Add ags as a bar for wms

---
+++ home-manager/desktop/bars/ags/default.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    ags.enable = lib.mkEnableOption "Enable ags bar";
+  };
+
+  config = lib.mkIf config.ags.enable {
+    programs.ags = {
+      enable = true;
+      # configDir = ../ags/config;
+      # configDir = config.home.homeDirectory + "/.config/ags";
+
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
+
+    home = {
+      packages = with pkgs; [
+        bun
+      ];
+
+      file = {
+        ".config/ags" = {
+          recursive = true;
+          source = ./config;
+        };
+        ".config/ags/types" = {
+          recursive = true;
+          source = config.lib.file.mkOutOfStoreSymlink "${config.programs.ags.finalPackage}/share/com.github.Aylur.ags/types";
+          # source = "${config.programs.ags.finalPackage}/share/com.github.Aylur.ags/types";
+        };
+      };
+    };
+  };
+}
