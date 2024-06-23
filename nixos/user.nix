55239c011a31eee0c238d408714085c573f1da6d
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-06-24 00:05:51 +0200

    Set stylix image dynamically from github

--- nixos/user.nix
+++ nixos/user.nix
@@ -7,19 +7,45 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     description = "Mirza";
-    extraGroups = ["dialout" "networkmanager" "wheel" "scanner" "lp" "video" "render"];
+    extraGroups = [
+      "dialout"
+      "networkmanager"
+      "wheel"
+      "scanner"
+      "lp"
+      "pipewire"
+      "audio"
+      "video"
+      "render"
+    ];
   };
 
   programs.zsh.enable = true;
 
   environment.sessionVariables.FLAKE = "/home/${username}/.config/flake";
 
+  # catppuccin = {
+  #   enable = true;
+  #   flavor = "macchiato";
+  #   accent = "green";
+  # };
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    image =
+      pkgs.fetchFromGitHub {
+        owner = "arunoruto";
+        repo = "wallpapers";
+        rev = "0814f66ec93e546c57bef0bdf5bf60cde401cf32";
+        hash = "sha256-sCVHHUK13BiN6c+13Ca1wQidmBvrDoGBuJGlg2pXuo4=";
+      }
+      + "/art/kanagawa/kanagawa-van-gogh.jpg";
     cursor = {
       name = "Catppuccin-Macchiato-Dark-Cursors";
       package = pkgs.catppuccin-cursors.macchiatoDark;
+    };
+    targets = {
+      lightdm.enable = true;
     };
   };
 }
