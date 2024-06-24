9081b89efc9ca9f8a44f7ba66c1e02c5c1a33134
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-06-24 17:55:46 +0200

    Config monitors

--- nixos/hosts/kyuubi/home.nix
+++ nixos/hosts/kyuubi/home.nix
@@ -2,4 +2,24 @@
   home.file = {
     ".config/monitors.xml".source = ./monitors.xml;
   };
+
+  wayland.windowManager.hyprland.settings.general = {
+    monitor = [
+      "DP-1,   1920x1080,0x0,1"
+      "DVI-I-1,1920x1200,-1200x-420,1,transform,1"
+    ];
+
+    workspace = [
+      "1,  monitor:DP-1"
+      "2,  monitor:DP-1"
+      "3,  monitor:DP-1"
+      "4,  monitor:DP-1"
+      "5,  monitor:DP-1"
+      "6,  monitor:DVI-I-1"
+      "7,  monitor:DVI-I-1"
+      "8,  monitor:DVI-I-1"
+      "9,  monitor:DVI-I-1"
+      "10, monitor:DVI-I-1"
+    ];
+  };
 }
