28c1a391c690afccd01ab329f1e51b663522a5b3
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-06-29 20:34:26 +0200

    Dont use extensions from nur, sync them with the profile

--- home-manager/pc/gui/firefox/default.nix
+++ home-manager/pc/gui/firefox/default.nix
@@ -10,16 +10,16 @@
 
     profiles.mirza = {
       # https://nur.nix-community.org/repos/rycee/
-      extensions = with config.nur.repos.rycee.firefox-addons; [
-        bitwarden
-        grammarly
-        mal-sync
-        ublock-origin
+      # extensions = with config.nur.repos.rycee.firefox-addons; [
+      #   bitwarden
+      #   grammarly
+      #   mal-sync
+      #   ublock-origin
 
-        augmented-steam
-        protondb-for-steam
-        steam-database
-      ];
+      #   augmented-steam
+      #   protondb-for-steam
+      #   steam-database
+      # ];
 
       settings = {
         "browser.startup.page" = 3;
