86f3dd20f6e6a1372db10233ba78e0d18ba7b37f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-06-29 20:35:31 +0200

    Temporary try the BROWSER env var

--- home-manager/pc/gui/default.nix
+++ home-manager/pc/gui/default.nix
@@ -1,7 +1,12 @@
-{
+{config, ...}: {
   imports = [
     ./firefox
     # ./steam.nix
     ./zathura.nix
   ];
+
+  # home.sessionVariables = {
+  #   # Firefox
+  #   BROWSER = "${config.programs.firefox.finalPackage}/bin/firefox";
+  # };
 }
