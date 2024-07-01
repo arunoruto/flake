93c47001088e849bada5da86960bbed90a2a059e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-02 01:18:49 +0200

    Add some nice additional formating for markup languages

--- home-manager/server/shell/helix/languages/markup.nix
+++ home-manager/server/shell/helix/languages/markup.nix
@@ -4,14 +4,44 @@
       language = [
         {
           name = "json";
-          auto-format = true;
+          # auto-format = true;
           # rulers = [120];
+          formatter = {
+            command = "prettier";
+            args = ["--parser" "json"];
+          };
+          language-servers = ["vscode-json-languageserver"];
+        }
+        {
+          name = "yaml";
+          # auto-format = true;
+          formatter = {
+            command = "prettier";
+            args = ["--parser" "yaml"];
+          };
+        }
+        {
+          name = "toml";
+          # auto-format = true;
+          # formatter = {
+          #   command = "prettier";
+          #   args = ["--plugin=${pkgs.nodePackages.prettier-plugin-toml}/lib/node_modules/prettier-plugin-toml" "--parser" "toml"];
+          # };
         }
       ];
       language-server = {
+        vscode-json-languageserver = {
+          command = "vscode-json-languageserver";
+          args = ["--stdio"];
+        };
       };
     };
     extraPackages = with pkgs; [
+      ansible-language-server
+      taplo
+      yaml-language-server
+      nodePackages.prettier
+      nodePackages.prettier-plugin-toml
       nodePackages.vscode-json-languageserver
     ];
   };
