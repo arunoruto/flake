601bf792c87c856b1e2d9b6f7e8e2a9dcca4fbf9
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-07-05 15:15:55 +0200

    Split UI theming from shell

--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -8,44 +8,8 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     image = "${config.home.homeDirectory}/Pictures/wallpapers/art/kanagawa/kanagawa-van-gogh.jpg";
-    cursor = {
-      name = "Catppuccin-Macchiato-Dark-Cursors";
-      package = pkgs.catppuccin-cursors.macchiatoDark;
-      size = 24;
-    };
-    fonts = {
-      serif = {
-        name = "Cantarell";
-        package = pkgs.cantarell-fonts;
-      };
-      # serif = {
-      #   name = "Roboto";
-      #   package = pkgs.roboto;
-      # };
-      sansSerif = config.stylix.fonts.serif;
-      # sansSerif = {
-      #   name = "Roboto Serif";
-      #   package = pkgs.roboto-serif;
-      # };
-      monospace = {
-        name = "FiraCode Nerd Font";
-        package = pkgs.fira-code-nerdfont;
-      };
-    };
-    opacity = {
-      terminal = 0.975;
-    };
     targets = {
-      firefox = {
-        enable = true;
-        profileNames = ["mirza"];
-      };
-      hyprland.enable = true;
-      # hyprpaper.enable = true;
-      # helix.enable = false;
-      kde.enable = false;
-      mako.enable = false;
-      nixvim.enable = false;
+      # nixvim.enable = false;
     };
   };
 }
