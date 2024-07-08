97f1231b4aa570543f1090942b589ea8a7232923
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-09 00:45:42 +0200

    startuptime.nvim

---
+++ home-manager/server/shell/nvim/plugins/utils/startuptime.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  programs.nixvim = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "vim-startuptime";
+        src = pkgs.fetchFromGitHub {
+          owner = "dstein64";
+          repo = "vim-startuptime";
+          rev = "v4.5.0";
+          hash = "sha256-hQ7/e7vEJx3j4CQfA6zkQFSe6wrFc9URZ2z47ZulW9A=";
+        };
+      })
+    ];
+  };
+}
