2653f54599ba7de38c5eb7e249ffb047925686eb
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-19 18:21:10 +0200

    Create devShell

--- flake.nix
+++ flake.nix
@@ -214,5 +214,17 @@
         };
       };
     };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.zlib
+      ];
+      shellHook = ''
+        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.zlib]}:''$LD_LIBRARY_PATH
+        # https://github.com/python-poetry/poetry/issues/8623#issuecomment-1793624371
+        export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+        echo "Flake Env"
+      '';
+    };
   };
 }
