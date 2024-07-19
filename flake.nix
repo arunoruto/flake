8e587a95b678bba7a482be22205ab6b6055690c2
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-19 02:44:36 +0200

    Comment out kde

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # Enable nur in the future
     nur.url = "github:nix-community/NUR";
     # Home Manager
