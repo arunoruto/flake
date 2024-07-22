cc4e055825294dd533be2ec2b9ac87f939d5e095
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-22 23:03:03 +0200

    Trying to run zed from pull request

--- nixos/pr.nix
+++ nixos/pr.nix
@@ -1,17 +1,22 @@
-{ config, pkgs, ... }:
-let
-  nixpkgs-tars = "https://github.com/NixOS/nixpkgs/archive/";
-in
 {
+  config,
+  pkgs,
+  ...
+}: let
+  nixpkgs-tars = "https://github.com/NixOS/nixpkgs/archive/";
+in {
   nixpkgs.config = {
     packageOverrides = pkgs: {
-      #pr284010 = import (fetchTarball
-      #  "${nixpkgs-tars}1dc8a2c39ec9e8f3b545c6dbb8fecbb89886d9af.tar.gz")
-      #    { config = config.nixpkgs.config; };
+      pr324549 =
+        import (fetchTarball {
+          url = "${nixpkgs-tars}fdef90a430d23487ea285197eb37338f6c97cbb1.tar.gz";
+          sha256 = "1id3k5shi2ffyqb4ph597bj5ybs1jfjk7sqrpn6ii4svlzlg33n5";
+        })
+        {config = config.nixpkgs.config;};
     };
   };
 
   environment.systemPackages = with pkgs; [
-    #pr284010.zed-editor
+    pr324549.zed-editor
   ];
 }
