900bd3c5ba4a93b9db493b23eec1151d97acb45f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-07-29 22:33:24 +0200

    Remove ld library path

--- flake.nix
+++ flake.nix
@@ -184,7 +184,7 @@
           inherit image;
           username = "mirza";
         };
-        modules = 
+        modules =
           nixos-modules
           ++ [
             ./nixos/hosts/kuchiki/configuration.nix
@@ -236,7 +236,7 @@
         zlib
       ];
       shellHook = ''
-        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.zlib]}:''$LD_LIBRARY_PATH
+        # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.zlib]}:''$LD_LIBRARY_PATH
         # https://github.com/python-poetry/poetry/issues/8623#issuecomment-1793624371
         export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
         echo "Flake Env"
