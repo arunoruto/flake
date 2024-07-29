0f0b9ef77051dce4e9515e4bdb360f8553e96783
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-07-29 05:46:56 +0200

    Modify newsboat

--- home-manager/server/programs/newsboat.nix
+++ home-manager/server/programs/newsboat.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{...}:
 #let
 #  catppuccin-newsboat = builtins.fetchGit {
 #    url = "https://github.com/catppuccin/newsboat";
@@ -9,66 +9,59 @@
   programs.newsboat = {
     enable = true;
     extraConfig = ''
-    include ~/.newsboat/dark
+      # include ~/.newsboat/dark
     '';
     urls = [
       {
         title = "Home Assistant";
-        tags = [ "smarthome" ];
+        tags = ["smarthome"];
         url = "https://home-assistant.io/atom.xml";
       }
       {
         title = "Home Assistant Alerts";
-        tags = [ "smarthome" ];
+        tags = ["smarthome"];
         url = "https://alerts.home-assistant.io/feed.xml";
       }
       {
         title = "Home Assistant Developer Docs Blog";
-        tags = [ "smarthome" ];
+        tags = ["smarthome"];
         url = "https://developers.home-assistant.io/blog/rss.xml";
       }
       {
         title = "Blog on Tailscale";
-        tags = [ "tailscale" ];
+        tags = ["tailscale"];
         url = "https://tailscale.com/blog/index.xml";
       }
       {
         title = "Changelog on Tailscale";
-        tags = [ "tailscale" ];
+        tags = ["tailscale"];
         url = "https://tailscale.com/changelog/index.xml";
       }
       {
         title = "Security Bulletins on Tailscale";
-        tags = [ "tailscale" ];
+        tags = ["tailscale"];
         url = "https://tailscale.com/security-bulletins/index.xml";
       }
       {
         title = "Tailscale Community";
-        tags = [ "tailscale" ];
+        tags = ["tailscale"];
         url = "https://tailscale.dev/feed.xml";
       }
       {
         title = "Framework Blog";
-        tags = [ "tech" ];
+        tags = ["tech"];
         url = "https://frame.work/de/en/blog.rss";
       }
       {
         title = "This Week in GNOME";
-        tags = [ "tech" ];
+        tags = ["tech"];
         url = "https://thisweek.gnome.org/index.xml";
       }
       {
         title = "Traefik Labs: Makes Networking Boring";
-        tags = [ "it" ];
+        tags = ["it"];
         url = "https://traefik.io/rss.xml";
       }
-      
-
-
     ];
   };
-
-  # Theme for alacritty
-  #home.file.".newsboat/latte".source = "${catppuccin-newsboat}/themes/latte";
-  #home.file.".newsboat/dark".source = "${catppuccin-newsboat}/themes/dark";
 }
