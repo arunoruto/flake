7d6e7b3c75e46ca18c542ab59a1501d93c92e500
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-08-05 04:50:18 +0200

    Dont let home-manager manage itself, it is managed now by nixos

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -16,7 +16,7 @@ in {
   };
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # programs.home-manager.enable = true;
 
   imports = [
     ./imports.nix
--- modules/home-manager/imports.nix
+++ modules/home-manager/imports.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./media
     ./pc
     ./server
   ];
