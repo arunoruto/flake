204900628695faabd1d1809bbb9e15b38df46dfa
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-08-05 15:15:48 +0200

    Make libreoffice a default option

--- modules/home-manager/pc/documents/libreoffice.nix
+++ modules/home-manager/pc/documents/libreoffice.nix
@@ -1,9 +1,18 @@
-{pkgs, ...}: {
-  home.packges = with pkgs; [
-    libreoffice-qt
-    hunspell
-    hunspellDicts.en-us
-    hunspellDicts.en-gb-ise
-    hunspellDicts.de-de
-  ];
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.libreoffice.enable = lib.mkEnableOption "Enable the document suite LibreOffice";
+
+  config = lib.mkIf config.libreoffice.enable {
+    home.packges = with pkgs; [
+      libreoffice-qt
+      hunspell
+      hunspellDicts.en-us
+      hunspellDicts.en-gb-ise
+      hunspellDicts.de-de
+    ];
+  };
 }
