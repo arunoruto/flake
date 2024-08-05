54bf23050343ee875f3914ce463e2f5b25064a32
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-08-05 04:51:24 +0200

    Make helix modular and try evil helix

--- modules/home-manager/server/shell/helix/default.nix
+++ modules/home-manager/server/shell/helix/default.nix
@@ -1,38 +1,53 @@
 # https://github.com/helix-editor/helix/wiki/Migrating-from-Vim
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./keys.nix
     ./languages
   ];
 
-  programs = {
-    helix = {
-      enable = true;
-      package = pkgs.unstable.helix;
-      settings = {
-        # theme = "catppuccin_macchiato";
-        # theme = "base16_transparent";
-        editor = {
-          true-color = true;
-          bufferline = "always";
-          line-number = "relative";
-          cursorline = true;
-          color-modes = true;
-          indent-guides = {
-            render = true;
+  options.helix.enable = lib.mkEnableOption "Enable the Helix editor";
+
+  config = lib.mkIf config.helix.enable {
+    programs = {
+      helix = {
+        enable = true;
+        # package = pkgs.unstable.helix;
+        package = pkgs.unstable.evil-helix;
+        settings = {
+          # theme = "catppuccin_macchiato";
+          # theme = "base16_transparent";
+          editor = {
+            true-color = true;
+            bufferline = "always";
+            line-number = "relative";
+            cursorline = true;
+            color-modes = true;
+            popup-border = "all";
+            indent-guides = {
+              render = true;
+            };
+            cursor-shape = {
+              normal = "block";
+              insert = "bar";
+              select = "underline";
+            };
+            # inline-diagnostics = {
+            #   cursor-line = "hint";
+            #   other-lines = "error";
+            # };
+            lsp.display-messages = true;
           };
-          cursor-shape = {
-            normal = "block";
-            insert = "bar";
-            select = "underline";
-          };
-          lsp.display-messages = true;
         };
+        ignores = [
+          ".build/"
+        ];
       };
-      ignores = [
-        ".build/"
-      ];
+      # zsh.shellAliases.vim = "hx";
     };
-    # zsh.shellAliases.vim = "hx";
   };
 }
