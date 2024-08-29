31ffa5f7a89e390a42f6e5443f4229e2419dbdf3
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-08-29 11:59:14 +0200

    Create a scss file for stylix

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -1,6 +1,7 @@
 # https://danth.github.io/stylix/
 {
   inputs,
+  config,
   pkgs,
   lib,
   theme,
@@ -27,5 +28,26 @@ in {
       nixvim.enable = false;
       vscode.enable = true;
     };
+  };
+
+  home.file = {
+    ".config/stylix/palette.scss".text = ''
+      $base00: ${config.lib.stylix.colors.withHashtag.base00};
+      $base01: ${config.lib.stylix.colors.withHashtag.base01};
+      $base02: ${config.lib.stylix.colors.withHashtag.base02};
+      $base03: ${config.lib.stylix.colors.withHashtag.base03};
+      $base04: ${config.lib.stylix.colors.withHashtag.base04};
+      $base05: ${config.lib.stylix.colors.withHashtag.base05};
+      $base06: ${config.lib.stylix.colors.withHashtag.base06};
+      $base07: ${config.lib.stylix.colors.withHashtag.base07};
+      $base08: ${config.lib.stylix.colors.withHashtag.base08};
+      $base09: ${config.lib.stylix.colors.withHashtag.base09};
+      $base0A: ${config.lib.stylix.colors.withHashtag.base0A};
+      $base0B: ${config.lib.stylix.colors.withHashtag.base0B};
+      $base0C: ${config.lib.stylix.colors.withHashtag.base0C};
+      $base0D: ${config.lib.stylix.colors.withHashtag.base0D};
+      $base0E: ${config.lib.stylix.colors.withHashtag.base0E};
+      $base0F: ${config.lib.stylix.colors.withHashtag.base0F};
+    '';
   };
 }
