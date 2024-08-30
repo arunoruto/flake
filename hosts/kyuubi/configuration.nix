faea5b8e074ba5172e0c3217e92e6c735f52b9d9
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-08-30 19:37:25 +0200

    Add vulkan support for kyuubi
    Still doesnt fix the warning in zed

--- hosts/kyuubi/configuration.nix
+++ hosts/kyuubi/configuration.nix
@@ -64,6 +64,18 @@ in {
         };
       };
     };
+
+    # OpenGL
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+      ];
+    };
   };
 
   systemd = {
