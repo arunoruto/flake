59dc70c1f6d9bfb7d401c07df6d18b3ac15f860a
Mirza <21687187+arunoruto@users.noreply.github.com>
2024-09-20 22:52:48 +0200

    Enable memtest86

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -6,7 +6,10 @@
   boot = {
     loader = {
       timeout = lib.mkDefault 0; # Hit F10 for a list of generations
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+      };
       efi.canTouchEfiVariables = true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
