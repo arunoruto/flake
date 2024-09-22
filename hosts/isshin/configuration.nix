2ddad97645f120a86d02937826895c33554bad99
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-09-22 21:37:29 +0200

    Add fix for framework 6ghz wifi

--- hosts/isshin/configuration.nix
+++ hosts/isshin/configuration.nix
@@ -7,6 +7,12 @@
 
   amd.enable = true;
 
+  # Fix 6GHz problem
+  # https://community.frame.work/t/responded-amd-rz616-wifi-card-doesnt-work-with-6ghz-on-kernel-6-7/43226
+  boot.extraModprobeConfig = ''
+    options mt7921_common disable_clc=1
+  '';
+
   # Framework specific kernel Params
   # boot = {
   #   kernelParams = [
