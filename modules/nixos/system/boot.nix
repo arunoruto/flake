85bdef7596e8e468225042db6ed8228044523060
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-13 13:18:39 +0000

    Make systemd kinda optional
    the bootloader needs to be made as a module, since not all systems have
    efi and grub is easier in that regard

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -7,10 +7,10 @@
     loader = {
       timeout = lib.mkDefault 0; # Hit F10 for a list of generations
       systemd-boot = {
-        enable = true;
+        enable = lib.mkDefault true;
         memtest86.enable = true;
       };
-      efi.canTouchEfiVariables = true;
+      efi.canTouchEfiVariables = lib.mkDefault true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
     #boot.plymouth = {
