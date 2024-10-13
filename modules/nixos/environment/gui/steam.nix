777715735b2fd27605788bfe9e3b59bde1a75dfb
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-13 15:37:28 +0200

    Enable port in firewall for local game transfers

--- modules/nixos/environment/gui/steam.nix
+++ modules/nixos/environment/gui/steam.nix
@@ -12,6 +12,7 @@
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall to share downloads locally with other devices
     };
   };
 }
