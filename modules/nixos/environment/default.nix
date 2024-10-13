b5f8a954f1d7afc39e3f4023c6732a554a52cf8c
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-13 13:19:30 +0000

    Disable cachix and latex for now

--- modules/nixos/environment/default.nix
+++ modules/nixos/environment/default.nix
@@ -9,6 +9,7 @@
     ./packages.nix
     ./cachix.nix
     ./fonts.nix
+    ./latex.nix
     ./ld.nix
     ./programming.nix
     ./python.nix
@@ -17,8 +18,9 @@
     ./intel.nix
   ];
 
-  cachix.enable = lib.mkDefault true;
+  cachix.enable = lib.mkDefault false;
   gui.enable = lib.mkDefault true;
+  latex.enable = lib.mkDefault false;
 
   amd.enable = lib.mkDefault false;
   intel.enable = lib.mkDefault false;
