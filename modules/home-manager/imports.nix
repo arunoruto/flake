5ba84c40e897ae3e71330b2ee887c11b0b9d968d
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-22 17:19:17 +0200

    Differentiate if the user is on a laptop. Default is false

--- modules/home-manager/imports.nix
+++ modules/home-manager/imports.nix
@@ -10,7 +10,10 @@
     ./theming
   ];
 
+  options.laptop.enable = lib.mkEnableOption "Sensible defaults for laptops";
+
   config = lib.mkIf (args ? nixosConfig) {
+    laptop.enable = lib.mkDefault osConfig.laptop.enable;
     pc.enable = lib.mkDefault osConfig.gui.enable;
 
     hostname = lib.mkDefault osConfig.networking.hostName;
