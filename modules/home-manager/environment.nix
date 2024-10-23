5d9d35bccda68d3a99870f6f94c658ab8437a539
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-24 01:43:25 +0200

    Cleanup env file

--- modules/home-manager/environment.nix
+++ modules/home-manager/environment.nix
@@ -7,16 +7,6 @@
   options.environment.enable = lib.mkEnableOption "Set env vars for user";
 
   config = lib.mkIf config.environment.enable {
-    # You can also manage environment variables but you will have to manually
-    # source
-    #
-    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  /etc/profiles/per-user/mar/etc/profile.d/hm-session-vars.sh
-    #
-    # if you don't want to manage your shell through Home Manager.
     home.sessionVariables = {
       # EDITOR = "nvim";
       EDITOR = "hx";
