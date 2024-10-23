4141f768afd23f82d54bff5c852e540e0f44c7d5
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-24 01:43:47 +0200

    vial is off per default

--- modules/nixos/services/input/default.nix
+++ modules/nixos/services/input/default.nix
@@ -9,5 +9,5 @@
   fingerprint.enable = lib.mkDefault false;
   kanata.enable = lib.mkDefault true;
   touchpad.enable = lib.mkDefault false;
-  vial.enable = lib.mkDefault true;
+  vial.enable = lib.mkDefault false;
 }
