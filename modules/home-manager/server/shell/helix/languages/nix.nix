c629c93944af3c959a7f7760083af6140705989c
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-27 21:27:37 +0100

    Format file

--- modules/home-manager/server/shell/helix/languages/nix.nix
+++ modules/home-manager/server/shell/helix/languages/nix.nix
@@ -1,23 +1,16 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
+{ pkgs, config, lib, ... }: {
   options.helix.nix.enable = lib.mkEnableOption "Helix Nix config";
 
   config = lib.mkIf config.helix.nix.enable {
     programs.helix = {
       languages = {
-        language = [
-          {
-            name = "nix";
-            auto-format = true;
-            formatter.command = "alejandra";
-            # formatter.command = "${pkgs.alejandra}/bin/alejandra";
-            language-servers = ["nixd" "nil"];
-          }
-        ];
+        language = [{
+          name = "nix";
+          auto-format = true;
+          formatter.command = "nixfmt";
+          # formatter.command = "${pkgs.alejandra}/bin/alejandra";
+          language-servers = [ "nixd" "nil" ];
+        }];
         language-server = {
           nixd = {
             command = "nixd";
@@ -27,6 +20,7 @@
       };
       extraPackages = with pkgs; [
         nixd
+        # nixfmt
         nil
         alejandra
       ];
