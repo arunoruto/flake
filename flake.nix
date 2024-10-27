03a6e9d0cf073ee461549ec007e7e3bb6e631536
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-27 21:26:55 +0100

    Add a facter config for isshin
    Config should be automated using facter in the future. Currently I will
    only test what it can provide and try to use it where it is possible.

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # NixOS
     nixos-hardware.url = "github:nixos/nixos-hardware";
+    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     nix-ld = {
       url = "github:Mic92/nix-ld";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -74,117 +75,138 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    lanzaboote,
-    nur,
-    home-manager,
-    ...
-  } @ inputs: let
-    # currently onlu x86 linux is used
-    # will maybe change in the future!
-    # -> look into flake parts/utils
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      # currently onlu x86 linux is used
+      # will maybe change in the future!
+      # -> look into flake parts/utils
+      system = "x86_64-linux";
 
-    # Some customization
-    # scheme = "catppuccin-macchiato";
-    # scheme = "tokyo-night-dark";
-    scheme = "gruvbox-material-dark-hard";
-    # image = "anime/jjk/satoru-gojo-jujutsu-kaisen-5k-ac.jpg";
-    # image = "anime/gruvbox/skull2.png";
-    image = "anime/gruvbox/boonies.png";
+      # Some customization
+      # scheme = "catppuccin-macchiato";
+      # scheme = "tokyo-night-dark";
+      scheme = "gruvbox-material-dark-hard";
+      # image = "anime/jjk/satoru-gojo-jujutsu-kaisen-5k-ac.jpg";
+      # image = "anime/gruvbox/skull2.png";
+      image = "anime/gruvbox/boonies.png";
 
-    hostname-users = {
-      # Personal
-      "isshin" = "mirza"; # Framework Laptop AMD 7040
-      "zangetsu" = "mirza"; # Framework Case Intel 11th
-      "yhwach" = "mirza"; # Tower PC
-      "kuchiki" = "mirza"; # New NAS Server
-      "yoruichi" = "mirza"; # Crappy AMD Mini PC
-      "kenpachi" = "mirza"; # S740 Mini PC
-      "narouter" = "mirza"; # Firewall
-      "aizen" = "mirza";
-      # Work
-      "kyuubi" = "mar"; # Crappy Work PC
-    };
-  in {
-    nixosConfigurations = nixpkgs.lib.genAttrs (builtins.attrNames hostname-users) (hostname:
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          username = hostname-users."${hostname}";
-        };
-        modules = [
-          nur.nixosModules.nur
-          {
-            networking.hostName = nixpkgs.lib.mkForce hostname;
-            nixpkgs = {
-              config.allowUnfree = true;
-              overlays = [
-                self.overlays.additions
-                self.overlays.unstable-packages
-              ];
-            };
-            theming = {
-              inherit scheme;
-              inherit image;
-            };
-          }
-          ./hosts/${hostname}
-          home-manager.nixosModules.home-manager
-          ./homes
-        ];
-      });
-
-    homeConfigurations = nixpkgs.lib.genAttrs (nixpkgs.lib.lists.unique (builtins.attrValues hostname-users)) (user:
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            self.overlays.unstable-packages
-          ];
-        };
-        modules = [
-          nur.hmModules.nur
-          ./modules/home-manager/home.nix
-          inputs.stylix.homeManagerModules.stylix
-          {
-            theming = {
-              inherit scheme;
-              inherit image;
-            };
-          }
-        ];
-        extraSpecialArgs = {
-          inherit inputs;
-          inherit user;
-        };
-      });
-
-    packages.${system} = import ./pkgs nixpkgs.legacyPackages.${system};
-    overlays = import ./overlays {inherit inputs;};
-
-    devShells.${system}.default = let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cmake
-          glib
-          stdenv.cc.cc.lib
-          zlib
-        ];
-
-        shellHook = ''
-          # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.glib pkgs.stdenv.cc.cc.lib pkgs.zlib]}:''$LD_LIBRARY_PATH
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.glib pkgs.stdenv.cc.cc.lib pkgs.zlib]}
-          # https://github.com/python-poetry/poetry/issues/8623#issuecomment-1793624371
-          export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-          echo "Flake Env"
-        '';
+      hostname-users = {
+        # Personal
+        "isshin" = "mirza"; # Framework Laptop AMD 7040
+        "zangetsu" = "mirza"; # Framework Case Intel 11th
+        "yhwach" = "mirza"; # Tower PC
+        "kuchiki" = "mirza"; # New NAS Server
+        "yoruichi" = "mirza"; # Crappy AMD Mini PC
+        "kenpachi" = "mirza"; # S740 Mini PC
+        "narouter" = "mirza"; # Firewall
+        "aizen" = "mirza";
+        # Work
+        "kyuubi" = "mar"; # Crappy Work PC
       };
-  };
+    in
+    {
+      nixosConfigurations = nixpkgs.lib.genAttrs (builtins.attrNames hostname-users) (
+        hostname:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            username = hostname-users."${hostname}";
+          };
+          modules = [
+            inputs.nur.nixosModules.nur
+            inputs.nixos-facter-modules.nixosModules.facter
+            {
+              networking.hostName = nixpkgs.lib.mkForce hostname;
+              facter.reportPath = nixpkgs.lib.mkIf (nixpkgs.lib.pathExists ./hosts/${hostname}/facter.json) ./hosts/${hostname}/facter.json;
+              nixpkgs = {
+                config.allowUnfree = true;
+                overlays = [
+                  self.overlays.additions
+                  self.overlays.unstable-packages
+                ];
+              };
+              theming = {
+                inherit scheme;
+                inherit image;
+              };
+            }
+            ./hosts/${hostname}
+            home-manager.nixosModules.home-manager
+            ./homes
+          ];
+        }
+      );
+
+      homeConfigurations =
+        nixpkgs.lib.genAttrs (nixpkgs.lib.lists.unique (builtins.attrValues hostname-users))
+          (
+            user:
+            home-manager.lib.homeManagerConfiguration {
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = [
+                  self.overlays.unstable-packages
+                ];
+              };
+              modules = [
+                inputs.nur.hmModules.nur
+                ./modules/home-manager/home.nix
+                inputs.stylix.homeManagerModules.stylix
+                {
+                  theming = {
+                    inherit scheme;
+                    inherit image;
+                  };
+                }
+              ];
+              extraSpecialArgs = {
+                inherit inputs;
+                inherit user;
+              };
+            }
+          );
+
+      packages.${system} = import ./pkgs nixpkgs.legacyPackages.${system};
+      overlays = import ./overlays { inherit inputs; };
+
+      devShells.${system}.default =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cmake
+            glib
+            stdenv.cc.cc.lib
+            zlib
+          ];
+
+          shellHook = ''
+            # export LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath [
+                pkgs.glib
+                pkgs.stdenv.cc.cc.lib
+                pkgs.zlib
+              ]
+            }:''$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath [
+                pkgs.glib
+                pkgs.stdenv.cc.cc.lib
+                pkgs.zlib
+              ]
+            }
+            # https://github.com/python-poetry/poetry/issues/8623#issuecomment-1793624371
+            export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+            echo "Flake Env"
+          '';
+        };
+    };
 }
