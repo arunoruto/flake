bcad2538de1efd3245dd35cc247f84fb0aa797e4
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-31 15:51:37 +0100

    configure the yubikey and associated modules

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -1,17 +1,23 @@
 {
   lib,
+  config,
   ...
 }:
 {
   imports = [
     ./fingerprint.nix
+    ./rssh.nix
     ./secrets.nix
     ./yubikey.nix
   ];
 
   fingerprint.enable = lib.mkDefault false;
+  rssh.enable = lib.mkDefault config.yubikey.enable;
   secrets.enable = lib.mkDefault true;
-  yubikey.enable = lib.mkDefault false;
+  yubikey = {
+    enable = lib.mkDefault false;
+    identifiers = { };
+  };
 
   security = {
     polkit.enable = true;
