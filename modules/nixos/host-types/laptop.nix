3dc300308188c8db538b8f71fc591dc1e06b1086
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-31 11:35:11 +0100

    Add yubikey support per default for laptops and workstations

--- modules/nixos/host-types/laptop.nix
+++ modules/nixos/host-types/laptop.nix
@@ -2,11 +2,13 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options.laptop.enable = lib.mkEnableOption "Sensible defaults for laptops";
 
   config = lib.mkIf config.laptop.enable {
     latex.enable = true;
     programming.enable = true;
+    yubikey.enable = true;
   };
 }
--- modules/nixos/host-types/workstation.nix
+++ modules/nixos/host-types/workstation.nix
@@ -2,12 +2,14 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options.workstation.enable = lib.mkEnableOption "Sensible defaults for workstations";
 
   config = lib.mkIf config.workstation.enable {
     latex.enable = true;
     programming.enable = true;
     upgrades.enable = true;
+    yubikey.enable = true;
   };
 }
