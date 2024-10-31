ec32e8581090a1f8b39af3a9f5f74cfb528f8fd4
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-10-31 12:14:06 +0100

    Add u2f secret

--- modules/nixos/security/secrets.nix
+++ modules/nixos/security/secrets.nix
@@ -2,17 +2,19 @@
   inputs,
   config,
   lib,
+  username,
   ...
-}: let
+}:
+let
+  user-conf = config.users.users.${username};
   secretspath = builtins.toString inputs.secrets;
-in {
+in
+{
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
 
-  options = {
-    secrets.enable = lib.mkEnableOption "Enable secrets managements using SOPS";
-  };
+  options.secrets.enable = lib.mkEnableOption "Enable secrets managements using SOPS";
 
   config = lib.mkIf config.secrets.enable {
     sops = {
@@ -20,7 +22,7 @@ in {
       validateSopsFiles = false;
 
       age = {
-        sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
         keyFile = "/var/lib/sops-nix/keys.txt";
         generateKey = true;
       };
@@ -29,8 +31,13 @@ in {
         # "private_keys/mirza@zangetsu" = {
         #   path = config.home.homeDirectory + "/.ssh/sops_key";
         # };
-        "tokens/copilot" = {};
-        "tokens/cachix" = {};
+        "tokens/copilot" = { };
+        "tokens/cachix" = { };
+        "yubico/u2f_keys" = {
+          owner = user-conf.name;
+          inherit (user-conf) group;
+          path = "${user-conf.home}/.config/yubico/u2f_keys";
+        };
       };
     };
   };
