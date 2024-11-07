f7647ee4dd8ebdd3a2b208fbdb671746bb5e9a53
arunoruto <mirza.arnaut45@gmail.com>
2024-11-07 03:59:10 +0100

    Add some aliases for git and always have signing on

--- modules/home-manager/server/programs/git.nix
+++ modules/home-manager/server/programs/git.nix
@@ -1,8 +1,16 @@
 # https://www.gerritcodereview.com/
 # https://meldmerge.org/
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   glab-pkg = pkgs.unstable.glab;
+  shellAliases = {
+    gns = "git -c commit.gpgsign=false";
+  };
 in
 {
   home.packages = [
@@ -34,13 +42,14 @@ in
         # user.signingkey = "6B890C16BB7F7971";
         user.signingkey = "${config.home.homeDirectory}/.ssh/id_tengen.pub";
         gpg.format = "ssh";
+        commit.gpgsign = true;
+        pull.rebase = true;
         "credential \"https://gitlab.com\"" = {
-          helper = "${glab-pkg}/bin/glab auth git-credential";
+          helper = "${lib.getExe glab-pkg} auth git-credential";
         };
         "credential \"https://gitlab.bv.e-technik.tu-dortmund.de\"" = {
-          helper = "${glab-pkg}/bin/glab auth git-credential";
+          helper = "${lib.getExe glab-pkg} auth git-credential";
         };
-        pull.rebase = true;
       };
     };
 
@@ -49,7 +58,6 @@ in
       gitCredentialHelper.enable = true;
       settings = {
         git_protocol = "https";
-        # editor = "hx";
         prompt = "enabled";
         aliases = {
           co = "pr checkout";
@@ -60,27 +68,40 @@ in
 
     lazygit = {
       enable = true;
-      settings = {
-        gui = {
-          theme = {
-            activeBorderColor = [
-              "#8aadf4"
-              "bold"
-            ];
-            inactiveBorderColor = [ "#a5adcb" ];
-            optionsTextColor = [ "#8aadf4" ];
-            selectedLineBgColor = [ "#363a4f" ];
-            cherryPickedCommitBgColor = [ "#494d64" ];
-            cherryPickedCommitFgColor = [ "#8aadf4" ];
-            unstagedChangesColor = [ "#ed8796" ];
-            defaultFgColor = [ "#cad3f5" ];
-            searchingActiveBorderColor = [ "#eed49f" ];
-          };
-          authorColors = {
-            "*" = "#b7bdf8";
-          };
-        };
-      };
+      # settings = {
+      #   gui = {
+      #     theme = {
+      #       activeBorderColor = [
+      #         "#8aadf4"
+      #         "bold"
+      #       ];
+      #       inactiveBorderColor = [ "#a5adcb" ];
+      #       optionsTextColor = [ "#8aadf4" ];
+      #       selectedLineBgColor = [ "#363a4f" ];
+      #       cherryPickedCommitBgColor = [ "#494d64" ];
+      #       cherryPickedCommitFgColor = [ "#8aadf4" ];
+      #       unstagedChangesColor = [ "#ed8796" ];
+      #       defaultFgColor = [ "#cad3f5" ];
+      #       searchingActiveBorderColor = [ "#eed49f" ];
+      #     };
+      #     authorColors = {
+      #       "*" = "#b7bdf8";
+      #     };
+      #   };
+      # };
     }; # // builtins.fromYAML (builtins.readFile (catppuccin-lazygit + themes-mergable/${flavour}/${colour}.yml));
+
+    bash = lib.mkIf config.programs.bash.enable {
+      inherit shellAliases;
+    };
+    fish = lib.mkIf config.programs.fish.enable {
+      inherit shellAliases;
+    };
+    nushell = lib.mkIf config.programs.nushell.enable {
+      inherit shellAliases;
+    };
+    zsh = lib.mkIf config.programs.zsh.enable {
+      inherit shellAliases;
+    };
   };
 }
