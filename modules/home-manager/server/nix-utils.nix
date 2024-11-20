5ce6ed8e50f969ab8f279345ea9611eae79e8150
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-11-20 16:44:25 +0100

    Waiting for new nix version to use pipes

--- modules/home-manager/server/nix-utils.nix
+++ modules/home-manager/server/nix-utils.nix
@@ -58,9 +58,9 @@ in
           "nix-command"
           "flakes"
         ];
-        extra-experimental-features = [
-          "pipe-operators"
-        ];
+        # extra-experimental-features = [
+        #   "pipe-operators"
+        # ];
         extra-substituters = [ "https://helix.cachix.org" ];
         extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
       };
