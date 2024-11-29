f2db412026c008ac11b205f1ff4cda6f997a5133
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-11-30 00:39:13 +0100

    Remove some not needed packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,21 +9,21 @@
 pkgs: rec {
   # banana-cursor = pkgs.callPackage ./banana-cursor/package.nix {};
   zluda-rocm5 = pkgs.callPackage ./zluda-rocm5/package.nix { };
-  candy-icons = pkgs.callPackage ./candy-icons/package.nix { };
+  # candy-icons = pkgs.callPackage ./candy-icons/package.nix { };
   mstm = pkgs.callPackage ./mstm/parallel.nix { };
   # zen-browser = pkgs.callPackage ./zen-browser/package.nix { };
-  wigxjpf = pkgs.callPackage ./wigxjpf/package.nix { };
-  pywigxjpf = pkgs.python3Packages.callPackage ./wigxjpf/pypackage.nix { };
+  # wigxjpf = pkgs.callPackage ./wigxjpf/package.nix { };
+  # pywigxjpf = pkgs.python3Packages.callPackage ./wigxjpf/pypackage.nix { };
 
   # Work testing
-  isis = pkgs.callPackage ./isis/package.nix {
-    inherit inja;
-    inherit ale;
-    inherit cspice;
-    inherit csm;
-  };
-  inja = pkgs.callPackage ./isis/inja.nix { };
-  ale = pkgs.callPackage ./ale/package.nix { };
-  cspice = pkgs.callPackage ./cspice/package.nix { };
-  csm = pkgs.callPackage ./csm/package.nix { };
+  # isis = pkgs.callPackage ./isis/package.nix {
+  #   inherit inja;
+  #   inherit ale;
+  #   inherit cspice;
+  #   inherit csm;
+  # };
+  # inja = pkgs.callPackage ./isis/inja.nix { };
+  # ale = pkgs.callPackage ./ale/package.nix { };
+  # cspice = pkgs.callPackage ./cspice/package.nix { };
+  # csm = pkgs.callPackage ./csm/package.nix { };
 }
