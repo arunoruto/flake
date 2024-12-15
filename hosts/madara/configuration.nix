45a5d78e0c34f5dfa272d24f41c2c357bc9dae86
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-12-15 14:56:39 +0100

    Testing madara for CI

--- hosts/madara/configuration.nix
+++ hosts/madara/configuration.nix
@@ -25,75 +25,33 @@ in
   runners.YASF.enable = true;
   nfs.enable = true;
 
-  # Disable Autosuspend for USB Bluetooth dongles
-  # boot = {
-  #   kernelModules = ["snd-hda-intel "];
-  #   extraModprobeConfig = ''
-  #     options btusb enable_autosuspend=n
-  #   '';
+  # hardware = {
+  #   # Make logitech devices work easier
+  #   logitech.wireless = {
+  #     enable = true;
+  #     enableGraphical = true;
+  #   };
+
+  #   # Tweaks for keychron
+  #   bluetooth = {
+  #     enable = true;
+  #     settings = {
+  #       General = {
+  #         FastConnect = true;
+  #         Experimental = true;
+  #       };
+  #       Policy = {
+  #         ReconnectAttempts = 7;
+  #         ReconnectIntervals = "1, 2, 3";
+  #       };
+  #     };
+  #   };
+
   # };
 
-  # Enable SSH Daemon
-  services = {
-    # openssh = {
-    #   enable = true;
-    #   # require public key authentication for better security
-    #   #settings.PasswordAuthentication = false;
-    #   #settings.KbdInteractiveAuthentication = false;
-    #   #settings.PermitRootLogin = "yes";
-    # };
-    # xrdp = {
-    #   enable = true;
-    #   defaultWindowManager = "gnome-remote-desktop";
-    #   openFirewall = true;
-    # };
-    gnome.gnome-remote-desktop.enable = true;
-  };
-
-  hardware = {
-    # Make logitech devices work easier
-    logitech.wireless = {
-      enable = true;
-      enableGraphical = true;
-    };
-
-    # Tweaks for keychron
-    bluetooth = {
-      enable = true;
-      settings = {
-        General = {
-          FastConnect = true;
-          Experimental = true;
-        };
-        Policy = {
-          ReconnectAttempts = 7;
-          ReconnectIntervals = "1, 2, 3";
-        };
-      };
-    };
-
-    # OpenGL
-    # opengl = {
-    #   enable = true;
-    #   driSupport = true;
-    #   driSupport32Bit = true;
-    #   extraPackages = with pkgs; [
-    #     vulkan-loader
-    #     vulkan-validation-layers
-    #     vulkan-extension-layer
-    #   ];
-    # };
-  };
-
-  systemd = {
-    tmpfiles.rules = [
-      "L+ /run/gdm/.config/monitors.xml - - - - ${monitorsConfig}"
-    ];
-
-    # services.NetworkManager-wait-online = {
-    #   serviceConfig = {
-    #     ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
-    #   };
-    # };
-  };
+  # systemd = {
+  #   tmpfiles.rules = [
+  #     "L+ /run/gdm/.config/monitors.xml - - - - ${monitorsConfig}"
+  #   ];
+  # };
 }
