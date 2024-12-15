86f1bef0064cfcc4e9b5f94fecbe1bceac2e2dc7
Mirza Arnaut <21687187+arunoruto@users.noreply.github.com>
2024-12-15 03:29:47 +0100

    Prepare bat for some tweaks in the future

--- modules/home-manager/server/cli/bat.nix
+++ modules/home-manager/server/cli/bat.nix
@@ -15,35 +15,37 @@ in
   options.bat.enable = lib.mkEnableOption "Enable cat rust alternative";
 
   config = lib.mkIf config.bat.enable {
-    programs = {
-      bat = {
-        enable = true;
-        config = {
-          #paging = "never";
-          #style = "plain";
-          # theme = "Monokai Extended";
-          #themes = "${catppuccin_bat}/Catppuccin-macchiato.tmTheme";
+    programs =
+      {
+        bat = {
+          enable = true;
+          # config = {
+          #   #paging = "never";
+          #   #style = "plain";
+          #   # theme = "Monokai Extended";
+          #   #themes = "${catppuccin_bat}/Catppuccin-macchiato.tmTheme";
+          # };
+          extraPackages = with pkgs.bat-extras; [
+            batdiff
+            batman
+            batgrep
+            batwatch
+          ];
         };
-        extraPackages = with pkgs.bat-extras; [
-          batdiff
-          batman
-          batgrep
-          batwatch
-        ];
+      }
+      // {
+        bash = {
+          inherit shellAliases;
+        };
+        fish = {
+          inherit shellAliases;
+        };
+        nushell = {
+          inherit shellAliases;
+        };
+        zsh = {
+          inherit shellAliases;
+        };
       };
-
-      bash = lib.mkIf config.programs.bash.enable {
-        inherit shellAliases;
-      };
-      fish = lib.mkIf config.programs.fish.enable {
-        inherit shellAliases;
-      };
-      nushell = lib.mkIf config.programs.nushell.enable {
-        inherit shellAliases;
-      };
-      zsh = lib.mkIf config.programs.zsh.enable {
-        inherit shellAliases;
-      };
-    };
   };
 }
