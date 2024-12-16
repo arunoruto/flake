2039fc8738d9afe6344ed0aaeda2657bb3f08a86
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-12-16 19:02:02 +0000

    Remove conda and ignore stuff for compinit if on non-NixOS system

--- modules/home-manager/server/shell/zsh.nix
+++ modules/home-manager/server/shell/zsh.nix
@@ -8,7 +8,7 @@
   pkgs,
   lib,
   ...
-}:
+}@args:
 {
   options.zsh.enable = lib.mkEnableOption "Configure zsh for you";
 
@@ -37,6 +37,8 @@
         };
         autosuggestion.enable = true;
         enableCompletion = true;
+        completionInit =
+          "autoload -U compinit && compinit" + lib.optionalString (!(args ? nixosConfig)) " -u";
         historySubstringSearch = {
           enable = true;
           searchDownKey = [ "^[OB" ];
@@ -60,7 +62,7 @@
           unsetopt BEEP
 
           # Enable autocomplete for . and ..
-          autoload -Uz compinit && compinit
+          #autoload -Uz compinit && compinit
           zstyle ':completion:*' special-dirs true
           zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
           zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
@@ -87,18 +89,18 @@
           # source ~/.config/zsh/_bws
 
           # >>> conda initialize >>>
-          __conda_setup="$('/opt/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-          if [ $? -eq 0 ]; then
-              eval "$__conda_setup"
-          else
-              if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
-                  . "/opt/conda/etc/profile.d/conda.sh"
-              else
-                  export PATH="/opt/conda/bin:$PATH"
-              fi
-          fi
-          unset __conda_setup
-          # <<< conda initialize <<<
+          # __conda_setup="$('/opt/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+          # if [ $? -eq 0 ]; then
+          #     eval "$__conda_setup"
+          # else
+          #     if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+          #         . "/opt/conda/etc/profile.d/conda.sh"
+          #     else
+          #         export PATH="/opt/conda/bin:$PATH"
+          #     fi
+          # fi
+          # unset __conda_setup
+          # # <<< conda initialize <<<
         '';
 
         # antidote = {
