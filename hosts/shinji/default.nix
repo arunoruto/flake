0fec91c4bf9a1d61e46304f096b157d3dd2de8a7
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-01-06 02:39:57 +0000

    Add coffee-lake config for shinji

--- hosts/shinji/default.nix
+++ hosts/shinji/default.nix
@@ -1,5 +1,7 @@
+{ inputs, ... }:
 {
   imports = [
+    (inputs.nixos-hardware.outPath + "/common/cpu/intel/coffee-lake")
     ./configuration.nix
     # ./disk.nix
     ./hardware-configuration.nix
