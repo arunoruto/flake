3ee2a3fb42b99152b19dd8566935a98f80a0c6c4
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-01-27 10:00:08 +0100

    Ghostty stylix config was backported

--- modules/home-manager/pc/terminal/ghostty.nix
+++ modules/home-manager/pc/terminal/ghostty.nix
@@ -5,29 +5,23 @@
   ...
 }:
 {
-  imports = [
-    # ./ghostty-module.nix
-    ./hm.nix
-  ];
 
-  config = {
-    programs.ghostty = {
-      package = pkgs.unstable.ghostty;
+  programs.ghostty = {
+    package = pkgs.unstable.ghostty;
 
-      enableBashIntegration = lib.mkDefault true;
-      enableFishIntegration = lib.mkDefault true;
-      enableZshIntegration = lib.mkDefault true;
+    enableBashIntegration = lib.mkDefault true;
+    enableFishIntegration = lib.mkDefault true;
+    enableZshIntegration = lib.mkDefault true;
 
-      settings = {
-        # background-blur-radius = 20;
-        window-height = config.terminals.height;
-        window-width = config.terminals.width;
-        keybind = [
-          "alt+enter=toggle_fullscreen"
-          "alt+h=previous_tab"
-          "alt+l=next_tab"
-        ];
-      };
+    settings = {
+      # background-blur-radius = 20;
+      window-height = config.terminals.height;
+      window-width = config.terminals.width;
+      keybind = [
+        "alt+enter=toggle_fullscreen"
+        "alt+h=previous_tab"
+        "alt+l=next_tab"
+      ];
     };
   };
 }
