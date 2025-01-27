30f70bd06bc92fb747da55f27afcbaba4a2ca541
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-01-27 10:00:22 +0100

    Add lsp config for shells

--- modules/home-manager/server/cli/helix/default.nix
+++ modules/home-manager/server/cli/helix/default.nix
@@ -8,8 +8,8 @@
 }@args:
 let
   nightly = !config.hosts.tinypc.enable && (args ? nixosConfig);
-  toml = pkgs.formats.toml { };
 in
+# toml = pkgs.formats.toml { };
 {
   imports = [
     ./keys.nix
@@ -19,87 +19,114 @@ in
   options.helix.enable = lib.mkEnableOption "Enable the Helix editor";
 
   config = lib.mkIf config.helix.enable {
-    programs = {
-      helix = {
-        enable = true;
-        # package = if nightly then inputs.helix.packages.${pkgs.system}.default else pkgs.unstable.helix;
-        # package = pkgs.helix;
-        package = pkgs.unstable.helix;
-        # package = pkgs.unstable.evil-helix;
-        settings = {
-          theme = lib.mkForce "my-stylix";
-          editor =
-            {
-              true-color = true;
-              bufferline = "always";
-              line-number = "relative";
-              rulers = [ 80 ];
-              cursorline = true;
-              color-modes = true;
-              popup-border = "all";
-              indent-guides = {
-                render = true;
-              };
-              cursor-shape = {
-                normal = "block";
-                insert = "bar";
-                select = "underline";
-              };
-              lsp = {
-                auto-signature-help = false;
-                display-messages = true;
-              };
-              file-picker = {
-                hidden = false;
-              };
-            }
-            // lib.optionalAttrs nightly {
-              end-of-line-diagnostics = "warning";
-              inline-diagnostics = {
-                cursor-line = "hint";
-                other-lines = "error";
-              };
+    programs.helix = {
+      enable = true;
+      # package = if nightly then inputs.helix.packages.${pkgs.system}.default else pkgs.unstable.helix;
+      package = pkgs.unstable.helix;
+      settings = {
+        theme = lib.mkForce "stylix-custom";
+        editor =
+          {
+            true-color = true;
+            bufferline = "always";
+            line-number = "relative";
+            rulers = [ 80 ];
+            cursorline = true;
+            color-modes = true;
+            popup-border = "all";
+            indent-guides = {
+              render = true;
             };
-        };
-        ignores = [
-          ".build/"
-        ];
+            cursor-shape = {
+              normal = "block";
+              insert = "bar";
+              select = "underline";
+            };
+            lsp = {
+              auto-signature-help = false;
+              display-messages = true;
+            };
+            file-picker = {
+              hidden = false;
+            };
+          }
+          // lib.optionalAttrs nightly {
+            end-of-line-diagnostics = "warning";
+            inline-diagnostics = {
+              cursor-line = "hint";
+              other-lines = "error";
+            };
+          };
       };
-      # zsh.shellAliases.vim = "hx";
+      ignores = [
+        ".build/"
+      ];
+      themes = {
+        stylix-custom = {
+          # inherits styling and pallet from the default stylix helix theme
+          inherits = "stylix";
+
+          # Normal Mode Indicator at the bottom left background base0D with base00 text
+          "ui.statusline.normal" = {
+            fg = "base00";
+            bg = "base0D";
+          };
+
+          # Makes the active buffer in the bufferline background base0D, foreground font is bold
+          "ui.bufferline.active" = {
+            fg = "base00";
+            bg = "base0D";
+            modifiers = [ "bold" ];
+          };
+
+          # Primary selection is base0D
+          "ui.cursor.primary" = {
+            fg = "base0D";
+            modifiers = [ "reversed" ];
+          };
+
+          # Matching brackets/quotes are underlined with color base0A
+          "ui.cursor.match" = {
+            # fg = "base0A";
+            # underline.style = "line";
+            modifiers = [ "reversed" ];
+          };
+        };
+      };
     };
 
-    home.file.".config/helix/themes/my-stylix.toml".source = toml.generate "my-stylix.toml" {
-      inherits = "stylix";
-      # "ui.selection" = {
-      #   fg = "base0A";
-      # };
-      # "ui.selection.primary" = {
-      #   fg = "base02";
-      # };
-      "ui.cursor" = {
-        fg = "base0B";
-        modifiers = [ "reversed" ];
-      };
-      "ui.cursor.primary" = {
-        fg = "base0A";
-        modifiers = [ "reversed" ];
-      };
-      "ui.cursor.match" = {
-        fg = "base0A";
-        modifiers = [ "underlined" ];
-      };
-      # ui.cursor = {
-      #   fg = "base0B";
-      #   modifiers = [ "reversed" ];
-      #   primary = {
-      #     fg = "base0A";
-      #     modifiers = [ "reversed" ];
-      #   };
-      #   match = {
-      #     fg = "base0A";
-      #     modifiers = [ "underlined" ];
-      #   };
-      # };
-    };
+    # home.file.".config/helix/themes/my-stylix.toml".source = toml.generate "my-stylix.toml" {
+    #   inherits = "stylix";
+    #   # "ui.selection" = {
+    #   #   fg = "base0A";
+    #   # };
+    #   # "ui.selection.primary" = {
+    #   #   fg = "base02";
+    #   # };
+    #   "ui.cursor" = {
+    #     fg = "base0B";
+    #     modifiers = [ "reversed" ];
+    #   };
+    #   "ui.cursor.primary" = {
+    #     fg = "base0A";
+    #     modifiers = [ "reversed" ];
+    #   };
+    #   "ui.cursor.match" = {
+    #     fg = "base0A";
+    #     modifiers = [ "underlined" ];
+    #   };
+    #   # ui.cursor = {
+    #   #   fg = "base0B";
+    #   #   modifiers = [ "reversed" ];
+    #   #   primary = {
+    #   #     fg = "base0A";
+    #   #     modifiers = [ "reversed" ];
+    #   #   };
+    #   #   match = {
+    #   #     fg = "base0A";
+    #   #     modifiers = [ "underlined" ];
+    #   #   };
+    #   # };
+    # };
   };
 }
--- modules/home-manager/server/cli/helix/languages/shells.nix
+++ modules/home-manager/server/cli/helix/languages/shells.nix
@@ -16,10 +16,30 @@
             auto-format = true;
             formatter.command = "shfmt";
           }
+          {
+            name = "fish";
+            auto-format = true;
+            formatter.command = "fish_indent";
+            language-servers = [ "fish-lsp" ];
+          }
+          {
+            name = "nu";
+            auto-format = true;
+            formatter.command = "nufmt";
+          }
         ];
+        language-server = {
+          fish-lsp = {
+            command = "fish-lsp";
+            args = [ "start" ];
+          };
+        };
       };
       extraPackages = with pkgs; [
         bash-language-server
+        fish
+        fish-lsp
+        nufmt
         shfmt
       ];
     };
