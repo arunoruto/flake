6fe7bf4bf55ce6a509a8de10852486c1f8840f01
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-01-31 01:49:29 +0100

    Clean up things a bit and add restrictions for tinypcs

--- homes/mar/default.nix
+++ homes/mar/default.nix
@@ -1,10 +1,11 @@
+{ config, lib, ... }:
 {
   imports = [
     ../../modules/home-manager/home.nix
   ];
 
   shell.main = "fish";
-  programs = {
+  programs = lib.optionalAttrs (!config.hosts.tinypc.enable) {
     fish.enable = true;
     nushell.enable = true;
     zsh.enable = true;
