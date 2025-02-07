bda2b172d195d3b76e6815ddd8be6b952e76ec21
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-02-07 17:22:51 +0100

    Add key option to beszel, so it can be used right away

--- modules/nixos/services/hardware/beszel.nix
+++ modules/nixos/services/hardware/beszel.nix
@@ -5,6 +5,8 @@
   ...
 }:
 {
+  meta.maintainers = with lib.maintainers; [ arunoruto ];
+
   options.services.beszel-agent = {
     enable = lib.mkEnableOption "Enable the beszel agent service";
     package = lib.mkPackageOption pkgs "beszel" { };
@@ -12,6 +14,11 @@
       type = lib.types.port;
       default = 45876;
       description = "The port for the beszel agent service";
+    };
+    key = lib.mkOption {
+      type = lib.types.str;
+      default = null;
+      description = "The raw value of the key";
     };
     keyFile = lib.mkOption {
       type = lib.types.path or lib.types.str;
@@ -32,7 +39,7 @@
         "error"
       ];
       default = "info";
-      description = "The log level for the beszel agent service. Valid values are debug, info, warn, error";
+      description = "The log level for the beszel agent service. Valid values are debug, info, warn, error.";
     };
   };
 
@@ -43,6 +50,7 @@
     lib.mkIf cfg.enable {
       systemd = {
         services.beszel-agent = {
+          # This ensures that nvidia-smi is in the path if GPU=true
           path = [ "/run/current-system/sw" ];
           unitConfig = {
             Description = "Beszel Agent Service";
@@ -53,7 +61,7 @@
             Environment = lib.lists.map (x: ''"${x}"'') [
               "LOG_LEVEL=${cfg.logLevel}"
               "PORT=${builtins.toString cfg.port}"
-              "KEY=${builtins.readFile cfg.keyFile}"
+              "KEY=${cfg.key}"
               "KEY_FILE=${cfg.keyFile}"
               "GPU=${if cfg.gpu then "true" else "false"}"
               "EXTRA_FILESYSTEMS=${lib.strings.concatStringsSep "," cfg.extraFilesystems}"
