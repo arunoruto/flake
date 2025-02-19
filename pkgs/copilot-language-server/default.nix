b9bec56c2f9eb6680ab2b0a22503ce96bf0f2249
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-02-19 09:05:00 +0100

    Update copilot to newest version and add hashes

--- pkgs/copilot-language-server/default.nix
+++ pkgs/copilot-language-server/default.nix
@@ -6,18 +6,24 @@
 
 buildNpmPackage rec {
   pname = "copilot-language-server";
-  version = "1.269.0";
+  version = "1.272.0";
+  # version = "1.269.0";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@github/copilot-language-server/-/copilot-language-server-${version}.tgz";
-    hash = "";
+    hash = "sha256-LzNwf9mEppk8AhwWM6U88thugoceOjUk9FBpPHi4O2M=";
   };
 
-  npmDepsHash = "";
+  npmDepsHash = "sha256-LiIVfW1PIEgx+tnsEgungxjYpWOCTaiB7qdIdCT8nmI=";
 
-  # postPatch = ''
-  #   ln -s ${./package-lock.json} package-lock.json
-  # '';
+  postPatch = ''
+    ln -s ${./package-lock.json} package-lock.json
+  '';
+
+  postInstall = ''
+    ls -la $out
+    ln -s $out/lib/node_modules/@github/copilot-language-server/dist $out/lib/node_modules/@github/dist
+  '';
 
   dontNpmBuild = true;
 
@@ -28,6 +34,7 @@ buildNpmPackage rec {
     homepage = "https://github.com/features/copilot";
     license = lib.licenses.cc-by-40;
     mainProgram = "copilot-language-server";
+    platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ arunoruto ];
   };
 }
