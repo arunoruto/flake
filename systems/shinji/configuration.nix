a29fb268488bfbf55fa8a94ea2d7f5eb8b887c78
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-11 23:18:49 +0000

    Add configs for shinji

--- systems/shinji/configuration.nix
+++ systems/shinji/configuration.nix
@@ -15,9 +15,16 @@
   bosflix.enable = true;
   # tpm.enable = true;
 
+  plex.enable = true;
+  services.plex.accelerationDevices = [ "/dev/dri/renderD128" ];
   services = {
     devmon.enable = true;
     udisks2.enable = true;
+    xrdp = {
+      enable = true;
+      defaultWindowManager = "kodi";
+    };
+    # x2goserver.enable = true;
   };
 
   security.pki.certificateFiles = [
