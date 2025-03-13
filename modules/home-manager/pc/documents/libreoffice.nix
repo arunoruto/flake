cd323de7bc966ca5e81ec907c40bd87c52f549a6
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-13 20:33:02 +0100

    Use latest version of libreoffice

--- modules/home-manager/pc/documents/libreoffice.nix
+++ modules/home-manager/pc/documents/libreoffice.nix
@@ -8,12 +8,16 @@
   options.libreoffice.enable = lib.mkEnableOption "Enable the document suite LibreOffice";
 
   config = lib.mkIf config.libreoffice.enable {
-    home.packages = with pkgs; [
-      libreoffice-qt
-      hunspell
-      hunspellDicts.en-us
-      hunspellDicts.en-gb-ise
-      hunspellDicts.de-de
-    ];
+    home.packages =
+      (with pkgs.unstable; [
+        libreoffice-fresh
+      ])
+      ++ (with pkgs; [
+        # libreoffice-qt
+        hunspell
+        hunspellDicts.en-us
+        hunspellDicts.en-gb-ise
+        hunspellDicts.de-de
+      ]);
   };
 }
