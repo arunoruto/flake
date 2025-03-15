e2229d296369ae7dcebeb199cfac88fe58277912
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-15 19:25:40 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-update.url = "github:nix-community/nixpkgs-update";
     # NixOS
     disko = {
       url = "github:nix-community/disko";
