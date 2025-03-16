98cd9965c8a1b5aa87e68172dac9dba3b697ca26
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-16 15:06:45 +0000

    Add beszel to kuchiki with rocm-smi suppor

--- systems/kuchiki/configuration.nix
+++ systems/kuchiki/configuration.nix
@@ -1,4 +1,9 @@
-{ ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   hosts.amd.enable = true;
   # display-manager.enable = lib.mkForce false;
@@ -17,14 +22,31 @@
     kernelModules = [ "amdgpu" ];
   };
 
-  services.hddfancontrol = {
-    enable = false;
-    disks = [
-      "/dev/sda"
-      "/dev/sdb"
-    ];
-    pwmPaths = [
-      "/sys/class/hwmon/hwmon2/pwm1"
-    ];
+  services = {
+    # hddfancontrol = {
+    #   enable = false;
+    #   disks = [
+    #     "/dev/sda"
+    #     "/dev/sdb"
+    #   ];
+    #   pwmPaths = [
+    #     "/sys/class/hwmon/hwmon2/pwm1"
+    #   ];
+    # };
+    beszel.agent = {
+      enable = true;
+      package = pkgs.unstable.beszel;
+      environment = {
+        LOG_LEVEL = "info";
+        GPU = "true";
+        KEY_FILE = config.sops.secrets."tokens/beszel-marvin".path;
+        EXTRA_FILESYSTEMS = lib.strings.concatStringsSep "," [
+          "nvme0n1p1"
+        ];
+      };
+      openFirewall = true;
+    };
   };
+
+  sops.secrets."tokens/beszel-marvin".mode = "0444";
 }
