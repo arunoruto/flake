af0eac7ba0edffa185a03dfbb76abb14efd91a0f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-16 21:53:57 +0000

    Update hardware config for kuchiki

--- systems/kuchiki/hardware-configuration.nix
+++ systems/kuchiki/hardware-configuration.nix
@@ -18,10 +18,7 @@
     "xhci_pci"
     "ahci"
     "nvme"
-    "usb_storage"
-    "usbhid"
     "sd_mod"
-    "sr_mod"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
@@ -48,9 +45,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0f0u7u3c2.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
