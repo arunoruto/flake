f6c9c6dee684083e99806eb58c7c65a19219d9ba
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-16 12:17:44 +0000

    Add module for condiguring auto mount of external drives

--- modules/nixos/media/default.nix
+++ modules/nixos/media/default.nix
@@ -7,15 +7,16 @@
   imports = [
     ./arr.nix
     ./bosflix.nix
+    ./external-drives.nix
     ./plex.nix
   ];
 
-  options = {
-    media.enable = lib.mkEnableOption "Enable media services";
-  };
+  options.media.enable = lib.mkEnableOption "Enable media services";
 
   config = lib.mkIf config.media.enable {
     arr.enable = lib.mkDefault true;
     plex.enable = lib.mkDefault true;
+
+    media.external-drives.enable = lib.mkDefault true;
   };
 }
---
+++ modules/nixos/media/external-drives.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+
+  options.media.external-drives.enable = lib.mkEnableOption "Mount external drives automagically";
+
+  config = lib.mkIf config.media.external-drives.enable {
+    services = {
+      devmon.enable = true;
+      udisks2.enable = true;
+      udev.extraRules = ''
+        # UDISKS_FILESYSTEM_SHARED
+        # ==1: mount filesystem to a shared directory (/media/VolumeName)
+        # ==0: mount filesystem to a private directory (/run/media/$USER/VolumeName)
+        # See udisks(8)
+        ENV{ID_FS_USAGE}=="filesystem|other|crypto", ENV{UDISKS_FILESYSTEM_SHARED}="1"
+      '';
+    };
+
+    systemd.tmpfiles.settings = {
+      "media"."/media".D = {
+        mode = "0755";
+        user = "root";
+        group = "root";
+      };
+    };
+  };
+}
