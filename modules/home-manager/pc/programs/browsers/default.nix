56d497366b9c0863446a969cdc2315c1ada58712
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-26 03:51:57 +0100

    Streamline chromium based browsers

--- modules/home-manager/pc/programs/browsers/default.nix
+++ modules/home-manager/pc/programs/browsers/default.nix
@@ -1,22 +1,60 @@
 {
   lib,
+  pkgs,
+  config,
+  inputs,
+  osConfig,
   ...
-}:
+}@args:
 {
   imports = [
     ./chrome.nix
     ./firefox
   ];
 
-  programs = {
-    brave.enable = lib.mkDefault false;
-    firefox.enable = lib.mkDefault true;
-    google-chrome.enable = lib.mkDefault true;
-    vivaldi.enable = lib.mkDefault false;
-  };
+  programs =
+    let
+      commandLineArgs =
+        [
+          "--enable-features=TouchpadOverscrollHistoryNavigation,UseOzonePlatform"
+        ]
+        ++ lib.optionals (args ? nixosConfig && !osConfig.hosts.nvidia.enable) [
+          "--ozone-platform=wayland"
+        ];
+      extensions = [
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden
+        { id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # ublock (normal)
+        { id = "kekjfbackdeiabghhcdklcdoekaanoel"; } # MAL-sync
+      ];
+      browserList = [
+        "brave"
+        "vivaldi"
+      ];
+      update =
+        lib.attrsets.recursiveUpdate
+          (lib.attrsets.genAttrs (browserList ++ [ "google-chrome" ]) (name: {
+            inherit commandLineArgs;
+          }))
+          (
+            lib.attrsets.genAttrs browserList (name: {
+              inherit extensions;
+            })
+          );
+    in
+    lib.attrsets.recursiveUpdate {
+      brave.enable = lib.mkDefault false;
+      firefox.enable = lib.mkDefault true;
+      google-chrome.enable = lib.mkDefault true;
+      vivaldi.enable = lib.mkDefault true;
+    } update;
 
-  # home.sessionVariables = {
-  #   # Firefox
-  #   BROWSER = "${config.programs.firefox.finalPackage}/bin/firefox";
-  # };
+  home = {
+    packages = [
+      inputs.zen-browser.packages.${pkgs.system}.default
+    ] ++ lib.optionals config.programs.vivaldi.enable [ pkgs.vivaldi-ffmpeg-codecs ];
+    # sessionVariables = {
+    #   # Firefox
+    #   BROWSER = "${config.programs.firefox.finalPackage}/bin/firefox";
+    # };
+  };
 }
