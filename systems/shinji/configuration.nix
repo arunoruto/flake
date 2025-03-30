fb1578f2ca789487b67cc142f78e853d11cfba45
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-30 23:05:09 +0000

    Fix drivePath for shinji

--- systems/shinji/configuration.nix
+++ systems/shinji/configuration.nix
@@ -14,7 +14,7 @@
   kodi.enable = true;
   bosflix = {
     enable = true;
-    drivePath = /media/86336459-5d8c-448e-93c3-f3e17c00d3b9;
+    drivePath = /media/86336459-5d8c-448e-93c3-f3e17c00d3b91;
   };
   # tpm.enable = true;
   media.external-drives.enable = true;
@@ -29,6 +29,10 @@
     ipv64-dyndns = {
       enable = true;
       domainKeyPath = config.sops.secrets."tokens/ipv64/orahovica".path;
+    };
+    qbittorrent = {
+      enable = true;
+      webuiPort = 8081;
     };
   };
 
