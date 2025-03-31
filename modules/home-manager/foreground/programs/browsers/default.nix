2c32e45418e71c35d9c84e3d9030386d37009b84
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-03-31 00:50:53 +0000

    Enable browser setup if foreground is enabled

--- modules/home-manager/foreground/programs/browsers/default.nix
+++ modules/home-manager/foreground/programs/browsers/default.nix
@@ -12,49 +12,51 @@
     ./firefox
   ];
 
-  programs =
-    let
-      commandLineArgs =
-        [
-          "--enable-features=TouchpadOverscrollHistoryNavigation,UseOzonePlatform"
-        ]
-        ++ lib.optionals (args ? nixosConfig && !osConfig.hosts.nvidia.enable) [
-          "--ozone-platform=wayland"
+  config = lib.mkIf config.foreground.enable {
+    programs =
+      let
+        commandLineArgs =
+          [
+            "--enable-features=TouchpadOverscrollHistoryNavigation,UseOzonePlatform"
+          ]
+          ++ lib.optionals (args ? nixosConfig && !osConfig.hosts.nvidia.enable) [
+            "--ozone-platform=wayland"
+          ];
+        extensions = [
+          { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden
+          { id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # ublock (normal)
+          { id = "kekjfbackdeiabghhcdklcdoekaanoel"; } # MAL-sync
         ];
-      extensions = [
-        { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden
-        { id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # ublock (normal)
-        { id = "kekjfbackdeiabghhcdklcdoekaanoel"; } # MAL-sync
-      ];
-      browserList = [
-        "brave"
-        "vivaldi"
-      ];
-      update =
-        lib.attrsets.recursiveUpdate
-          (lib.attrsets.genAttrs (browserList ++ [ "google-chrome" ]) (name: {
-            inherit commandLineArgs;
-          }))
-          (
-            lib.attrsets.genAttrs browserList (name: {
-              inherit extensions;
-            })
-          );
-    in
-    lib.attrsets.recursiveUpdate {
-      brave.enable = lib.mkDefault false;
-      firefox.enable = lib.mkDefault true;
-      google-chrome.enable = lib.mkDefault true;
-      vivaldi.enable = lib.mkDefault true;
-    } update;
+        browserList = [
+          "brave"
+          "vivaldi"
+        ];
+        update =
+          lib.attrsets.recursiveUpdate
+            (lib.attrsets.genAttrs (browserList ++ [ "google-chrome" ]) (name: {
+              inherit commandLineArgs;
+            }))
+            (
+              lib.attrsets.genAttrs browserList (name: {
+                inherit extensions;
+              })
+            );
+      in
+      lib.attrsets.recursiveUpdate {
+        brave.enable = lib.mkDefault false;
+        firefox.enable = lib.mkDefault true;
+        google-chrome.enable = lib.mkDefault true;
+        vivaldi.enable = lib.mkDefault true;
+      } update;
 
-  home = {
-    packages = [
-      inputs.zen-browser.packages.${pkgs.system}.default
-    ] ++ lib.optionals config.programs.vivaldi.enable [ pkgs.vivaldi-ffmpeg-codecs ];
-    # sessionVariables = {
-    #   # Firefox
-    #   BROWSER = "${config.programs.firefox.finalPackage}/bin/firefox";
-    # };
+    home = {
+      packages = [
+        inputs.zen-browser.packages.${pkgs.system}.default
+      ] ++ lib.optionals config.programs.vivaldi.enable [ pkgs.vivaldi-ffmpeg-codecs ];
+      # sessionVariables = {
+      #   # Firefox
+      #   BROWSER = "${config.programs.firefox.finalPackage}/bin/firefox";
+      # };
+    };
   };
 }
