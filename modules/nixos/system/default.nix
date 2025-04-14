456b9ad7fec7d074c9720149142a47c829d0447d
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-04-14 02:34:33 +0200

    Add tags for systems (should replace host types!)

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -2,6 +2,7 @@
   inputs,
   pkgs,
   lib,
+  config,
   # image,
   # scheme,
   ...
@@ -24,29 +25,45 @@
     ./nvidia
   ];
 
-  nix-utils.enable = lib.mkDefault true;
-  secureboot.enable = lib.mkDefault false;
-  upgrades.enable = lib.mkDefault false;
-
-  hosts = {
-    amd.enable = lib.mkDefault false;
-    intel.enable = lib.mkDefault false;
-    nvidia.enable = lib.mkDefault false;
+  options.system.tags = lib.mkOption {
+    # type = lib.types.listOf lib.types.str;
+    type = with lib.types; listOf str;
+    default = [ ];
+    description = ''
+      List of tags for this system. This can be used to identify
+      this system in scripts or other tools.
+    '';
   };
 
-  environment.systemPackages = with pkgs; [
-    inputs.colmena.packages.${pkgs.system}.colmena
-    deploy-rs
-    lsof
-    lshw
-    tree
-  ];
+  config =
+    {
+      nix-utils.enable = lib.mkDefault true;
+      secureboot.enable = lib.mkDefault false;
+      upgrades.enable = lib.mkDefault false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+      hosts = {
+        amd.enable = lib.mkDefault false;
+        intel.enable = lib.mkDefault false;
+        nvidia.enable = lib.mkDefault false;
+      };
+
+      environment.systemPackages = with pkgs; [
+        deploy-rs
+        lsof
+        lshw
+        tree
+      ];
+
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It‘s perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "23.11"; # Did you read the comment?
+    }
+    // lib.optionalAttrs (inputs ? colmena) {
+      colmena.deployment.tags = config.system.tags;
+      environment.systemPackages = [ inputs.colmena.packages.${pkgs.system}.colmena ];
+    };
 }
