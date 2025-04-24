9004c8d2256864ec81076900a26285b342a4763e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-04-24 17:18:16 +0200

    Enable steelseries aerox 5 in libratbag

--- modules/nixos/services/hardware/mouse.nix
+++ modules/nixos/services/hardware/mouse.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  # devices = pkgs.copyPathToStore ./libratbag-devices;
+  devices = pkgs.copyPathToStore ./libratbag-devices;
 in
 {
   options.hardware.mouse.enable = lib.mkEnableOption "Enable mouse config";
@@ -13,13 +13,13 @@ in
   config = lib.mkIf config.hardware.mouse.enable {
     services.ratbagd = {
       enable = true;
-      # package = pkgs.libratbag.overrideAttrs (old: {
-      #   postInstall =
-      #     (old.postInstall or "")
-      #     + ''
-      #       install -Dm444 ${devices} $out/share/libratbag
-      #     '';
-      # });
+      package = pkgs.libratbag.overrideAttrs (old: {
+        postInstall =
+          (old.postInstall or "")
+          + ''
+            install -Dm444 ${devices}/* $out/share/libratbag
+          '';
+      });
     };
     environment.systemPackages = [ pkgs.piper ];
   };
