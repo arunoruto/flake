7699196ba716a157e7079f4904bb5ad1d9f517ee
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-05-08 01:59:31 +0200

    chore: add zenmap

--- modules/nixos/environment/packages.nix
+++ modules/nixos/environment/packages.nix
@@ -2,28 +2,30 @@
 {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    clinfo
-    ffmpeg
-    file
-    git
-    htop
-    imagemagickBig
-    iperf
-    killall
-    nmap
-    ntfs3g
-    usbutils
-    pciutils
-    powertop
-    tlrc
-    traceroute
-    unzip
+  environment.systemPackages =
+    (with pkgs; [
+      clinfo
+      ffmpeg
+      file
+      git
+      htop
+      imagemagickBig
+      iperf
+      killall
+      nmap
+      ntfs3g
+      usbutils
+      pciutils
+      powertop
+      tlrc
+      traceroute
+      unzip
 
-    dust
-    riffdiff
-    # unstable.ventoy
-    vim
-    wget
-  ];
+      dust
+      riffdiff
+      # unstable.ventoy
+      vim
+      wget
+    ])
+    ++ (with pkgs.unstable; [ zenmap ]);
 }
