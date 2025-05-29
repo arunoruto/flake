c8f5d1ac80ddbbcdb9b8e8c1aa5a1ff6c95bb403
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-05-30 01:26:44 +0200

    chore: comment out zluda-rocm5, since rocm6 is the default now for 25.05 and up

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,7 +15,7 @@ rec {
   # dpcpp-bin = pkgs.callPackage ./dpcpp/bin.nix { };
   # dpcpp-prop = pkgs.callPackage ./dpcpp/proprietary.nix { };
   # banana-cursor = pkgs.callPackage ./banana-cursor/package.nix {};
-  zluda-rocm5 = pkgs.callPackage ./zluda-rocm5/package.nix { };
+  # zluda-rocm5 = pkgs.callPackage ./zluda-rocm5/package.nix { };
   # candy-icons = pkgs.callPackage ./candy-icons/package.nix { };
   mstm = pkgs.callPackage ./mstm { enableMPI = false; };
   teaftp = pkgs.callPackage ./teaftp/package.nix { };
