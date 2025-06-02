3b1f4a524396724a66d1988ff1fb613f51175c0f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-06-02 15:48:16 +0200

    pkgs: unibear with autopatch

--- pkgs/unibear/bin.nix
+++ pkgs/unibear/bin.nix
@@ -3,67 +3,68 @@
   stdenvNoCC,
   fetchurl,
   nix-update-script,
-  # versionCheckHook,
+  autoPatchelfHook,
 
-  testers,
+  libgcc,
 }:
-
 let
-  os-arch =
-    {
-      aarch64-darwin = "unibear-darwin_aarch64-apple-darwin";
-      aarch64-linux = "unibear-linux_aarch64-unknown-linux-gnu";
-      x86_64-darwin = "unibear-darwin_x86_64-apple-darwin";
-      x86_64-linux = "unibear-linux_x86_64-unknown-linux-gnu";
-    }
-    ."${stdenvNoCC.hostPlatform.system}"
+  os-arch-options = {
+    aarch64-darwin = {
+      name = "unibear-darwin_aarch64-apple-darwin";
+      hash = "sha256-kYfL+ou4SlZChiaNZOscSzGZSIdiZpJ3QnGBPgtpJ5U=";
+    };
+    aarch64-linux = {
+      name = "unibear-linux_aarch64-unknown-linux-gnu";
+      hash = "sha256-BYu4wvv80yThc9O3pUMCAGJQkaVzlVbgIvIXUf6hhro=";
+    };
+    x86_64-darwin = {
+      name = "unibear-darwin_x86_64-apple-darwin";
+      hash = "sha256-u7wDsHiFyRyQiIr05hMmtbEABzRuJsLFLPZdY1SKmG4=";
+    };
+    x86_64-linux = {
+      name = "unibear-linux_x86_64-unknown-linux-gnu";
+      hash = "sha256-4ahYZvdrRBIeI9+xV6SL+03JQALiq83hGmq5ZEZnIPA=";
+    };
+  };
+  name-hash =
+    os-arch-options."${stdenvNoCC.hostPlatform.system}"
       or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
-
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "unibear";
   version = "0.4.2";
 
   src = fetchurl {
-    url = "https://github.com/kamilmac/unibear/releases/download/v${finalAttrs.version}/${os-arch}";
-    hash = "sha256-4ahYZvdrRBIeI9+xV6SL+03JQALiq83hGmq5ZEZnIPA=";
-    # inherit (finalAttrs) pname;
-    # name = finalAttrs.pname;
-    # stripRoot = false;
+    url = "https://github.com/kamilmac/unibear/releases/download/v${finalAttrs.version}/${name-hash.name}";
+    inherit (name-hash) hash;
   };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  builInputs = [ libgcc.lib ];
+
+  preBuild = ''
+    addAutoPatchelfSearchPath ${lib.makeLibraryPath [ libgcc ]}
+  '';
 
   installPhase = ''
     runHook preInstall
 
-    install "$src" -Dm755 "$out"/bin/${finalAttrs.pname}
+    install "$src" -Dm755 "$out"/bin/${finalAttrs.meta.mainProgram}
 
     runHook postInstall
   '';
 
-  dontUnpack = true;
-
-  passthru = {
-    updateScript = nix-update-script { };
-    tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
-  };
-
-  # nativeInstallCheckInputs = [ versionCheckHook ];
-  # versionCheckProgram = "${placeholder "out"}/bin/${finalAttrs.pname}";
-  # versionCheckProgramArg = [ "--version" ];
-  # doInstallCheck = true;
+  passthru.updateScript = nix-update-script { };
 
   meta = {
-    description = "A lean TUI AI assistant: run your tools, stay in control, no magic tricks.";
+    description = "Lean TUI AI assistant";
     homepage = "https://github.com/kamilmac/unibear";
-    licence = lib.licenses.mit;
-    mainProgram = finalAttrs.pname;
-    platforms = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-      "arm64-apple-darwin"
-    ];
+    license = lib.licenses.mit;
+    mainProgram = "unibear";
+    platforms = lib.attrsets.mapAttrsToList (name: value: name) os-arch-options;
     maintainers = with lib.maintainers; [ arunoruto ];
   };
 })
