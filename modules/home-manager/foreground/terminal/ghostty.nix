063dafdfa0ee67e506e2fc46b59604449353c82b
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-10 03:12:14 +0200

    ghostty: new kernel doesn't fix the slowdown...

--- modules/home-manager/foreground/terminal/ghostty.nix
+++ modules/home-manager/foreground/terminal/ghostty.nix
@@ -2,19 +2,30 @@
   config,
   pkgs,
   lib,
+  osConfig,
   ...
-}:
+}@args:
 {
-
   programs.ghostty = {
-    package = pkgs.unstable.ghostty;
-    # package = pkgs.unstable.ghostty.overrideAttrs (_: {
-    #   preBuild = ''
-    #     shopt -s globstar
-    #     sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-    #     shopt -u globstar
-    #   '';
-    # });
+    # package = pkgs.unstable.ghostty;
+    package =
+      if
+        (
+          (args ? nixosConfig)
+          && (builtins.elem osConfig.boot.kernelPackages.kernel.version (
+            lib.lists.forEach [ "4" "5" ] (x: "6.15." + x)
+          ))
+        )
+      then
+        (pkgs.unstable.ghostty.overrideAttrs (_: {
+          preBuild = ''
+            shopt -s globstar
+            sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+            shopt -u globstar
+          '';
+        }))
+      else
+        pkgs.unstable.ghostty;
 
     enableBashIntegration = lib.mkDefault true;
     enableFishIntegration = lib.mkDefault true;
