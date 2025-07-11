2a36daa66946e0a8254af62c6f204b52609de6e1
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-11 02:40:55 +0200

    ghostty: use unstable version, needed restart

--- modules/home-manager/foreground/terminal/ghostty.nix
+++ modules/home-manager/foreground/terminal/ghostty.nix
@@ -7,25 +7,25 @@
 }@args:
 {
   programs.ghostty = {
-    # package = pkgs.unstable.ghostty;
-    package =
-      if
-        (
-          (args ? nixosConfig)
-          && (builtins.elem osConfig.boot.kernelPackages.kernel.version (
-            lib.lists.forEach [ "4" "5" ] (x: "6.15." + x)
-          ))
-        )
-      then
-        (pkgs.unstable.ghostty.overrideAttrs (_: {
-          preBuild = ''
-            shopt -s globstar
-            sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-            shopt -u globstar
-          '';
-        }))
-      else
-        pkgs.unstable.ghostty;
+    package = pkgs.unstable.ghostty;
+    # package =
+    #   if
+    #     (
+    #       (args ? nixosConfig)
+    #       && (builtins.elem osConfig.boot.kernelPackages.kernel.version (
+    #         lib.lists.forEach [ "4" "5" ] (x: "6.15." + x)
+    #       ))
+    #     )
+    #   then
+    #     (pkgs.unstable.ghostty.overrideAttrs (_: {
+    #       preBuild = ''
+    #         shopt -s globstar
+    #         sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+    #         shopt -u globstar
+    #       '';
+    #     }))
+    #   else
+    #     pkgs.unstable.ghostty;
 
     enableBashIntegration = lib.mkDefault true;
     enableFishIntegration = lib.mkDefault true;
