c782967d66293b0364bbf7e6f78eba2d2c038645
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-21 18:26:10 +0200

    lib: add a traefikConfig function to make a quick config

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,5 @@
 lib: {
   systemConfig = import ../systems/lib.nix lib;
   arr = import ../modules/nixos/media/arr/lib.nix lib;
+  networking = import ../modules/nixos/services/network/lib.nix lib;
 }
---
+++ modules/nixos/services/network/lib.nix
@@ -0,0 +1,32 @@
+lib: {
+  traefikConfig =
+    {
+      serviceName,
+      url,
+      port,
+      path ? "/",
+      cert ? "cf",
+    }:
+    let
+      host =
+        if (builtins.isList url) then
+          lib.strings.concatStringsSep " || " (lib.lists.forEach url (x: "Host(`${x}`)"))
+        else
+          "Host(`${url}`)";
+    in
+    {
+      http = {
+        routers."${serviceName}" = {
+          rule = "(${host}) && PathPrefix(`/${path}`)";
+          tls.certresolver = cert;
+          entrypoints = [ "websecure" ];
+          service = serviceName;
+        };
+        services."${serviceName}".loadbalancer.servers = [
+          {
+            url = "http://localhost:${builtins.toString port}";
+          }
+        ];
+      };
+    };
+}
