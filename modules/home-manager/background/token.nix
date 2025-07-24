41dee7ca21a095519e6b61e443829fa83155dcee
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-24 17:49:46 +0200

    hm: remove tokens, Gemini Token sucks...

--- modules/home-manager/background/token.nix
+++ modules/home-manager/background/token.nix
@@ -1,17 +1,17 @@
 { config, ... }:
 {
-  programs =
-    let
-      gemini-token = "cat ${config.sops.secrets."tokens/gemini".path}";
-    in
-    {
-      zsh.initContent = ''
-        export GEMINI_API_KEY="$(${gemini-token})"
-      '';
-      fish.interactiveShellInit = ''
-        set -gx GEMINI_API_KEY $(${gemini-token})
-      '';
-    };
+  # programs =
+  #   let
+  #     gemini-token = "cat ${config.sops.secrets."tokens/gemini".path}";
+  #   in
+  #   {
+  #     zsh.initContent = ''
+  #       export GEMINI_API_KEY="$(${gemini-token})"
+  #     '';
+  #     fish.interactiveShellInit = ''
+  #       set -gx GEMINI_API_KEY $(${gemini-token})
+  #     '';
+  #   };
 
-  sops.secrets."tokens/gemini" = { };
+  # sops.secrets."tokens/gemini" = { };
 }
