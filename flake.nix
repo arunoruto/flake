e0156c2613cfaa43a651d2cb45419fc89f129623
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-07-24 18:19:31 +0200

    flake: check also for formatting with nixfmt

--- flake.nix
+++ flake.nix
@@ -224,9 +224,9 @@
           checks = {
             pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
               src = ./.;
-              # hooks = {
-              #   nixfmt-rfc-style.enable = true;
-              # };
+              hooks = {
+                nixfmt-rfc-style.enable = true;
+              };
             };
           };
         }
