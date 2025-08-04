8dc9cfbc13ea8044b2a872b45ace1f3f351b0465
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-08-04 02:58:32 +0200

    hm: add mods

--- modules/home-manager/background/programs/default.nix
+++ modules/home-manager/background/programs/default.nix
@@ -7,11 +7,13 @@
 {
   imports = [
     ./git.nix
+    ./mods.nix
     ./newsboat.nix
     ./papis.nix
     ./pop.nix
   ];
 
+  programs.mods.enable = !config.hosts.tinypc.enable;
   programs.pop.enable = !config.hosts.tinypc.enable;
   home.packages = with pkgs; [
     # bws
