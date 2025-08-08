2dbb95c6130e0ca7daa5ca1119a211bb26db3878
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-08-08 02:10:36 +0200

    systems: setup sado

--- systems/sado/configuration.nix
+++ systems/sado/configuration.nix
@@ -1,5 +1,48 @@
 { ... }:
 {
-  hosts.tinypc.enable = true;
-  colmena.deployment.buildOnTarget = true;
+  # colmena.deployment.buildOnTarget = true;
+  hosts = {
+    tinypc.enable = true;
+    intel = {
+      enable = true;
+      gpu.enable = true;
+    };
+    zfs.enable = true;
+  };
+  systemd.services.zfs-mount.enable = false;
+  networking.hostId = "b9b3aa87";
+
+  display-manager.enable = false;
+  desktop-environment.enable = false;
+  programs.enable = false;
+
+  services = {
+    media = {
+      enable = true;
+      dataDir = "/mnt/flash/appdata";
+      openFirewall = true;
+    };
+    tailscale.tsidp = {
+      enable = true;
+      port = 41443;
+      localPort = 41080;
+    };
+    immich = {
+      enable = true;
+      # dataDir = "/mnt/flash/appdata/paperless";
+      # environment.UPLOAD_LOCATION = "/mnt/flash/photos";
+      mediaLocation = "/mnt/flash/photos";
+    };
+    paperless = {
+      enable = true;
+      dataDir = "/mnt/flash/appdata/paperless";
+      mediaDir = "/mnt/flash/documents";
+    };
+    scrutiny.collector = {
+      enable = true;
+      settings.api.endpoint = "https://scrutiny.bv.e-technik.tu-dortmund.de";
+    };
+    traefik.enable = true;
+    syncthing.enable = true;
+  };
 }
--- systems/sado/default.nix
+++ systems/sado/default.nix
@@ -1,5 +1,8 @@
+{ inputs, ... }:
 {
   imports = [
+    # inputs.nixos-hardware.nixosModules.common.cpu.intel.alder-lake
+    "${inputs.nixos-hardware.outPath}/common/cpu/intel/alder-lake"
     ./configuration.nix
     ./hardware-configuration.nix
   ];
