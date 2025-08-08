a51fff7c566e3d2df81f4d507525c17363e14c17
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-08-08 02:11:47 +0200

    flake: update secrets

--- systems/isshin/default.nix
+++ systems/isshin/default.nix
@@ -1,7 +1,6 @@
 { inputs, ... }:
 {
   imports = [
-    # inputs.nixos-hardware.nixosModules.framework-11th-gen-intel
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
 
     ./configuration.nix
