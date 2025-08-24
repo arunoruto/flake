88d1b01910325a9a6cd7e38958d757d701418d4e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-08-24 17:54:01 +0200

    systems: add ssd and gpu config for sado

--- systems/sado/default.nix
+++ systems/sado/default.nix
@@ -3,6 +3,8 @@
   imports = [
     # inputs.nixos-hardware.nixosModules.common.cpu.intel.alder-lake
     "${inputs.nixos-hardware.outPath}/common/cpu/intel/alder-lake"
+    "${inputs.nixos-hardware.outPath}/common/gpu/intel/alder-lake"
+    "${inputs.nixos-hardware.outPath}/common/pc/ssd"
     ./configuration.nix
     ./hardware-configuration.nix
   ];
