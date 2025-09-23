b5b083b8b8b5651a500d59929d7a33c335888d96
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-09-23 14:01:45 +0200

    pkgs: beszel: use finalAttrs and set extraArgs for auto update

--- pkgs/beszel/package.nix
+++ pkgs/beszel/package.nix
@@ -5,19 +5,19 @@
   nix-update-script,
   buildNpmPackage,
 }:
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "beszel";
   version = "0.12.6";
 
   src = fetchFromGitHub {
     owner = "henrygd";
     repo = "beszel";
-    tag = "v${version}";
     hash = "sha256-OZD8nB2oKaMFvUbDfYNhtq18riaQSdTZASDUJ29TYu8=";
+    hash = "sha256-6hqRlttuDU5mAqgClc5I+lSx+XHpNPuOLirbVEA08/g=";
   };
 
   webui = buildNpmPackage {
-    inherit
+    inherit (finalAttrs)
       pname
       version
       src
@@ -45,18 +45,20 @@ buildGoModule rec {
       runHook postInstall
     '';
 
-    sourceRoot = "${src.name}/beszel/site";
+    sourceRoot = "${finalAttrs.src.name}/internal/site";
 
     npmDepsHash = "sha256-6J1LwRzwbQyXVBHNgG7k8CQ67JZIDqYreDbgfm6B4w4=";
   };
 
-  sourceRoot = "${src.name}/beszel";
+  # sourceRoot = "${src.name}";
 
   vendorHash = "sha256-NEJ93Q4yzMu9CN9LVK88ytfCwhe8Gz//lPAsx/Y3VRY=";
 
+  postPatch = ''substituteInPlace go.mod --replace "go 1.25.1" "go 1.25.0"'';
+
   preBuild = ''
     mkdir -p site/dist
-    cp -r ${webui}/* site/dist
+    cp -r ${finalAttrs.webui}/* site/dist
   '';
 
   postInstall = ''
@@ -64,13 +66,18 @@ buildGoModule rec {
     mv $out/bin/hub $out/bin/beszel-hub
   '';
 
-  passthru.updateScript = nix-update-script { };
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--subpackage"
+      "webui"
+    ];
+  };
 
   meta = {
     homepage = "https://github.com/henrygd/beszel";
-    changelog = "https://github.com/henrygd/beszel/releases/tag/v${version}";
+    changelog = "https://github.com/henrygd/beszel/releases/tag/v${finalAttrs.version}";
     description = "Lightweight server monitoring hub with historical data, docker stats, and alerts";
     maintainers = with lib.maintainers; [ bot-wxt1221 ];
     license = lib.licenses.mit;
   };
-}
+})
