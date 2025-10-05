dce9ab9298acff881290b533c040655f80c02cb6
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-10-05 22:35:03 +0200

    hm: clean up ghostty

--- modules/home-manager/foreground/terminal/ghostty.nix
+++ modules/home-manager/foreground/terminal/ghostty.nix
@@ -2,42 +2,22 @@
   config,
   pkgs,
   lib,
-  osConfig,
   ...
-}@args:
+}:
 {
   programs.ghostty = {
     package = pkgs.unstable.ghostty;
-    # package =
-    #   if
-    #     (
-    #       (args ? nixosConfig)
-    #       && (builtins.elem osConfig.boot.kernelPackages.kernel.version (
-    #         lib.lists.forEach [ "4" "5" ] (x: "6.15." + x)
-    #       ))
-    #     )
-    #   then
-    #     (pkgs.unstable.ghostty.overrideAttrs (_: {
-    #       preBuild = ''
-    #         shopt -s globstar
-    #         sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-    #         shopt -u globstar
-    #       '';
-    #     }))
-    #   else
-    #     pkgs.unstable.ghostty;
 
     enableBashIntegration = lib.mkDefault true;
-    enableFishIntegration = lib.mkDefault true;
-    enableZshIntegration = lib.mkDefault true;
+    enableFishIntegration = lib.mkDefault config.programs.fish.enable;
+    enableZshIntegration = lib.mkDefault config.programs.zsh.enable;
 
     settings = {
-      # background-blur-radius = 20;
       background-blur = 20;
       window-height = config.terminals.height;
       window-width = config.terminals.width;
       keybind = [
-        "global:ctrl+backquote=toggle_quick_terminal"
+        # "global:ctrl+backquote=toggle_quick_terminal"
         "alt+enter=toggle_fullscreen"
         "alt+h=previous_tab"
         "alt+l=next_tab"
