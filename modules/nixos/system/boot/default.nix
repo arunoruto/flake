57bbd810e16cb1a2a40356d6476e497464976c6c
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-11 04:38:56 +0100

    fix: some things are not for arm

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -16,7 +16,7 @@
       efi.canTouchEfiVariables = lib.mkDefault true;
       timeout = lib.mkDefault 0; # Hit F10 for a list of generations
     };
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkOverride 1001 pkgs.linuxPackages_latest;
     # plymouth.enable = true;
   };
 }
--- modules/nixos/system/boot/systemd-boot.nix
+++ modules/nixos/system/boot/systemd-boot.nix
@@ -1,13 +1,14 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf config.boot.loader.systemd-boot.enable {
     boot.loader = {
       systemd-boot = {
-        memtest86.enable = true;
+        memtest86.enable = (pkgs.system == "x86_64-linux");
       };
       efi.canTouchEfiVariables = lib.mkDefault true;
     };
