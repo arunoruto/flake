3194da26c4aff60ff5cde5aff3679e2422ac1c1b
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-19 23:49:29 +0100

    pkgs: use stable ollama

--- modules/nixos/services/ai.nix
+++ modules/nixos/services/ai.nix
@@ -24,13 +24,21 @@
 
       ollama = {
         enable = true;
-        package = pkgs.unstable.ollama;
+        # package = pkgs.ollama;
+        # package = pkgs.unstable.ollama;
         # if config.hosts.nvidia.enable then
         #   pkgs.unstable.ollama-cuda
         # else if config.hosts.amd.enable then
         #   pkgs.unstable.ollama-rocm
         # else
         #   pkgs.unstable.ollama;
+        package =
+          if config.hosts.nvidia.enable then
+            pkgs.ollama-cuda
+          else if config.hosts.amd.enable then
+            pkgs.ollama-rocm
+          else
+            pkgs.ollama-vulkan;
         host = "0.0.0.0";
         loadModels = [
           "deepseek-r1:14b"
