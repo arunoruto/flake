9a4c17d529aa31abc1985534d97424a11444d185
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-28 05:09:03 +0100

    pkgs: build docs-mcp-server

---
+++ packages/top-level/docs-mcp-server/package.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  makeWrapper,
+  playwright,
+}:
+buildNpmPackage (finalAttrs: {
+  pname = "docs-mcp-server";
+  version = "1.29.0";
+
+  src = fetchFromGitHub {
+    owner = "arabold";
+    repo = "docs-mcp-server";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-eQzKziJCy963uRQKtdjj1Ob8U4iG1Z1folso2K0Xx/I=";
+  };
+
+  npmDepsHash = "sha256-uSzsXBpzFthF9klmsTcNpkhVDroT0KuOHduXSxUJaeQ=";
+
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ playwright ];
+
+  postInstall =
+    let
+      browsers = (builtins.fromJSON (builtins.readFile "${playwright}/browsers.json")).browsers;
+      chromium-rev = (builtins.head (builtins.filter (x: x.name == "chromium") browsers)).revision;
+    in
+    ''
+      wrapProgram $out/bin/docs-mcp-server \
+        --set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD 1 \
+        --set PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH "${playwright.browsers}/chromium-${chromium-rev}/chrome-linux/chrome"
+    '';
+
+  meta = {
+    description = "Grounded Docs MCP Server: Enhance Your AI Coding Assistant ";
+    homepage = "https://grounded.tools/";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ arunoruto ];
+  };
+})
