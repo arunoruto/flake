0f32755eff7d1a22fd917a9570988a7f7b2c71c1
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-29 22:17:51 +0100

    hm: add ollama to opencode

--- modules/home-manager/background/cli/ai/default.nix
+++ modules/home-manager/background/cli/ai/default.nix
@@ -18,8 +18,39 @@
       opencode = {
         enable = true;
         package = pkgs.unstable.opencode;
+        enableMcpIntegration = true;
         settings = {
           theme = "stylix";
+          tools = {
+            bash = true;
+            edit = true;
+            write = true;
+            read = true;
+            grep = true;
+            glob = true;
+            list = true;
+          };
+          provider.llama = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "Ollama";
+            options.baseURL = "http://madara.king-little.ts.net:11434/v1";
+            models = {
+              "gemma3:12b" = {
+                name = "Gemma3";
+                tool_call = false;
+              };
+              "deepseek-r1:14b" = {
+                name = "DeepSeek-R1";
+                # tools = true;
+                reasoning = true;
+              };
+              "qwen3:14b" = {
+                name = "Qwen3";
+                tools = true;
+                reasoning = true;
+              };
+            };
+          };
         };
       };
     };
