80653feec391f538a5b7e88985db1a844f60dafb
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-30 02:20:02 +0100

    nixos: media: update immich and paperless to prepare with oidc

--- modules/nixos/media/immich.nix
+++ modules/nixos/media/immich.nix
@@ -2,9 +2,16 @@
   lib,
   pkgs,
   config,
+  # inputs,
   ...
 }:
+# let
+#   module = "services/web-apps/immich.nix";
+# in
 {
+  # disabledModules = [ module ];
+  # imports = [ "${inputs.nixpkgs-unstable.outPath}/nixos/modules/${module}" ];
+
   config = lib.mkIf config.services.immich.enable {
     services =
       let
@@ -12,31 +19,34 @@
       in
       {
         immich = {
-          # environment = {
-          #   UPLOAD_LOCATION = lib.mkDefault "${cfg.dataDir}/photos";
-          # };
+          # package = pkgs.unstable.immich;
+          environment = {
+            IMMICH_VERSION = "2.3.1";
+            IMMICH_LOG_LEVEL = "debug";
+            # UPLOAD_LOCATION = lib.mkDefault "${cfg.dataDir}/photos";
+          };
           host = "0.0.0.0";
           mediaLocation = lib.mkDefault "${cfg.dataDir}/photos";
           accelerationDevices = lib.mkDefault null;
-          settings = {
-            oauth = lib.mkDefault {
-              issuerUrl = "http://localhost:${builtins.toString config.services.tailscale.tsidp.localPort}";
-              clientId = "unused";
-              clientSecret = "unused";
-              scope = "openid email profile";
-              signingAlgorithm = "RS256";
-              storageLabelClaim = "preferred_username";
-              storageQuotaClaim = "immich_quota";
-              defaultStorageQuota = null;
-              buttonText = "Tailscale Identity Provider";
-              enabled = false;
-              profileSigningAlgorithm = "none";
-              autoRegister = true;
-              autoLaunch = false;
-              mobileOverrideEnabled = false;
-              mobileRedirectUri = "";
-            };
-          };
+          # settings = {
+          #   oauth = lib.mkDefault {
+          #     issuerUrl = "http://id.arnaut.me";
+          #     clientId._secret = config.sops.secrets."services/immich/client-id".path;
+          #     clientSecret._secret = config.sops.secrets."services/immich/client-secret".path;
+          #     scope = "openid email profile";
+          #     signingAlgorithm = "RS256";
+          #     storageLabelClaim = "preferred_username";
+          #     storageQuotaClaim = "immich_quota";
+          #     defaultStorageQuota = null;
+          #     buttonText = "Pocket-ID";
+          #     enabled = true;
+          #     profileSigningAlgorithm = "none";
+          #     autoRegister = true;
+          #     autoLaunch = false;
+          #     mobileOverrideEnabled = false;
+          #     mobileRedirectUri = "";
+          #   };
+          # };
           openFirewall = lib.mkDefault cfg.openFirewall;
         };
         # tailscale.serve = {
@@ -70,5 +80,49 @@
     users.users.immich.extraGroups = lib.optionals (config.users.groups ? "media") [
       config.users.groups.media.name
     ];
+    sops = {
+      secrets = {
+        "services/immich/client-id" = { };
+        "services/immich/client-secret" = { };
+        # "config/maileroo/server" = { };
+        # "config/maileroo/username" = { };
+        # "config/maileroo/password" = { };
+      };
+      # templates."${paperless-oidc}".content = ''
+      #   PAPERLESS_EMAIL_HOST=${config.sops.placeholder."config/maileroo/server"}
+      #   PAPERLESS_EMAIL_PORT=587
+      #   PAPERLESS_EMAIL_USE_TLS=true
+      #   PAPERLESS_EMAIL_USE_SSL=false
+      #   PAPERLESS_EMAIL_HOST_USER=${config.sops.placeholder."config/maileroo/username"}
+      #   PAPERLESS_EMAIL_HOST_PASSWORD=${config.sops.placeholder."config/maileroo/password"}
+      #   PAPERLESS_SOCIALACCOUNT_PROVIDERS=${
+      #     builtins.toJSON {
+      #       openid_connect = {
+      #         OAUTH_PKCE_ENABLED = true;
+      #         SCOPE = [
+      #           "openid"
+      #           "profile"
+      #           "email"
+      #         ];
+      #         APPS = [
+      #           {
+      #             provider_id = "pocket-id";
+      #             name = "Pocket-ID";
+      #             client_id = config.sops.placeholder."services/paperless/client-id";
+      #             secret = config.sops.placeholder."services/paperless/client-secret";
+      #             settings.server_url = "https://id.arnaut.me";
+      #           }
+      #           {
+      #             provider_id = "tsidp";
+      #             name = "Tailscale Identity Provider";
+      #             client_id = "unused";
+      #             secret = "unused";
+      #             settings.server_url = "http://localhost:${builtins.toString config.services.tailscale.tsidp.localPort}";
+      #           }
+      #         ];
+      #       };
+      #     }
+      #   }'';
+    };
   };
 }
--- modules/nixos/media/paperless.nix
+++ modules/nixos/media/paperless.nix
@@ -2,11 +2,16 @@
   lib,
   pkgs,
   config,
-  # inputs,
+  inputs,
   ...
 }:
+let
+  paperless-oidc = "pocket-id-config.env";
+in
 {
-  # imports = [ "${inputs.nixpkgs-unstable.outPath}/nixos/modules/services/misc/paperless.nix" ];
+  disabledModules = [ "services/misc/paperless.nix" ];
+  imports = [ "${inputs.nixpkgs-unstable.outPath}/nixos/modules/services/misc/paperless.nix" ];
+  # imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/hardware/cpu/amd-microcode.nix" ];
 
   options.services.paperless.openFirewall = lib.mkEnableOption "Open firewall for Paperless";
 
@@ -21,8 +26,8 @@
             cfg = config.services.media;
           in
           {
-            # package = lib.mkDefault pkgs.unstable.paperless-ngx;
-            package = lib.mkDefault pkgs.paperless-ngx;
+            package = lib.mkDefault pkgs.unstable.paperless-ngx;
+            # package = lib.mkDefault pkgs.paperless-ngx;
             dataDir = lib.mkDefault "${cfg.dataDir}/paperless";
             configureTika = lib.mkDefault true;
             address = if config.services.paperless.openFirewall then "0.0.0.0" else "127.0.0.1";
@@ -31,40 +36,19 @@
               PAPERLESS_URL = "https://${url}";
               PAPERLESS_FORCE_SCRIPT_NAME = lib.mkDefault "/paperless";
               PAPERLESS_APPS = lib.mkDefault "allauth.socialaccount.providers.openid_connect";
-              PAPERLESS_SOCIALACCOUNT_PROVIDERS = lib.mkDefault (
-                builtins.toJSON {
-                  openid_connect = {
-                    OAUTH_PKCE_ENABLED = true;
-                    SCOPE = [
-                      "openid"
-                      "profile"
-                      "email"
-                    ];
-                    APPS = [
-                      {
-                        provider_id = "tsidp";
-                        name = "Tailscale Identity Provider";
-                        client_id = "unused";
-                        secret = "unused";
-                        settings = {
-                          server_url = "http://localhost:${builtins.toString config.services.tailscale.tsidp.localPort}";
-                        };
-                      }
-                    ];
-                  };
-                }
-              );
+              PAPERLESS_CSRF_TRUSTED_ORIGINS = lib.strings.concatStringsSep "," [
+                "https://services.arnaut.me"
+                "https://${url}"
+              ];
             };
+            environmentFile = config.sops.templates."${paperless-oidc}".path;
           };
         traefik.dynamicConfigOptions = lib.networking.traefikConfig {
           serviceName = "paperless";
-          # url = "kuchiki.sparrow-yo.ts.net";
           inherit url;
-          # url = config.services.paperless.settings.PAPERLESS_URL;
           inherit (config.services.paperless) port;
           path = config.services.paperless.settings.PAPERLESS_FORCE_SCRIPT_NAME;
         };
-
       };
     networking.firewall.allowedTCPPorts = lib.mkIf config.services.paperless.openFirewall [
       config.services.paperless.port
@@ -72,5 +56,50 @@
     # users.users.paperless.extraGroups = lib.optionals (config.users.groups ? "media") [
     #   config.users.groups.media.name
     # ];
+
+    sops = {
+      secrets = {
+        "services/paperless/client-id" = { };
+        "services/paperless/client-secret" = { };
+        "config/maileroo/server" = { };
+        "config/maileroo/username" = { };
+        "config/maileroo/password" = { };
+      };
+      templates."${paperless-oidc}".content = ''
+        PAPERLESS_EMAIL_HOST=${config.sops.placeholder."config/maileroo/server"}
+        PAPERLESS_EMAIL_PORT=587
+        PAPERLESS_EMAIL_USE_TLS=true
+        PAPERLESS_EMAIL_USE_SSL=false
+        PAPERLESS_EMAIL_HOST_USER=${config.sops.placeholder."config/maileroo/username"}
+        PAPERLESS_EMAIL_HOST_PASSWORD=${config.sops.placeholder."config/maileroo/password"}
+        PAPERLESS_SOCIALACCOUNT_PROVIDERS=${
+          builtins.toJSON {
+            openid_connect = {
+              OAUTH_PKCE_ENABLED = true;
+              SCOPE = [
+                "openid"
+                "profile"
+                "email"
+              ];
+              APPS = [
+                {
+                  provider_id = "pocket-id";
+                  name = "Pocket-ID";
+                  client_id = config.sops.placeholder."services/paperless/client-id";
+                  secret = config.sops.placeholder."services/paperless/client-secret";
+                  settings.server_url = "https://id.arnaut.me";
+                }
+                {
+                  provider_id = "tsidp";
+                  name = "Tailscale Identity Provider";
+                  client_id = "unused";
+                  secret = "unused";
+                  settings.server_url = "http://localhost:${builtins.toString config.services.tailscale.tsidp.localPort}";
+                }
+              ];
+            };
+          }
+        }'';
+    };
   };
 }
