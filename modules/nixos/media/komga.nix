7f90b7515ab82195be5a5ba41407ed98e2e0dc8e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-12-08 02:29:15 +0100

    nixos: services: configure komga

--- modules/nixos/media/komga.nix
+++ modules/nixos/media/komga.nix
@@ -5,7 +5,104 @@
 }:
 {
   config = lib.mkIf config.services.komga.enable {
-    services.komga = {
+    services = {
+      komga = {
+        settings = {
+          server.port = 25600;
+          komga = {
+            oauth2-account-creation = true;
+            oidc-email-verification = false;
+          };
+          spring.security.oauth2.client = {
+            registration.pocket-id = {
+              provider = "pocket-id";
+              client-id = "\${KOMGA_OAUTH_CLIENT_ID}";
+              client-secret = "\${KOMGA_OAUTH_CLIENT_SECRET}";
+              client-name = "Pocket-ID";
+              scope = "openid,email,profile";
+              authorization-grant-type = "authorization_code";
+              redirect-uri = "{baseUrl}/login/oauth2/code/{registrationId}";
+              client-authentication-method = "client_secret_post";
+            };
+            provider.pocket-id = {
+              user-name-attribute = "email";
+              issuer-uri = "https://id.arnaut.me";
+            };
+          };
+        };
+      };
+      cloudflared.tunnels."${config.networking.hostName}".ingress = [
+        {
+          hostname = "library.${config.services.cloudflared.defaultDomain}";
+          service = "http://localhost:${builtins.toString config.services.komga.settings.server.port}";
+        }
+      ];
     };
+    sops = {
+      secrets = {
+        "services/komga/client-id".owner = config.services.komga.user;
+        "services/komga/client-secret".owner = config.services.komga.user;
+      };
+      templates."komga.env" = {
+        content = ''
+          KOMGA_OAUTH_CLIENT_ID=${config.sops.placeholder."services/komga/client-id"}
+          KOMGA_OAUTH_CLIENT_SECRET=${config.sops.placeholder."services/komga/client-secret"}
+        '';
+        owner = config.services.komga.user;
+      };
+    };
+    systemd.services.komga.serviceConfig.EnvironmentFile = config.sops.templates."komga.env".path;
   };
+  # config = lib.mkMerge [
+  #   (lib.mkIf config.services.komga.enable {
+  #     services.komga = {
+  #       settings = {
+  #         server.port = 8080;
+  #         komga = {
+  #           oauth2-account-creation = true;
+  #           oidc-email-verification = false;
+  #         };
+  #         spring.security.oauth2.client = {
+  #           registration.pocket-id = {
+  #             provider = "pocket-id";
+  #             client-id = "\${KOMGA_OAUTH_CLIENT_ID}";
+  #             client-secret = "\${KOMGA_OAUTH_CLIENT_SECRET}";
+  #             client-name = "Pocket-ID";
+  #             scope = "openid,email,profile";
+  #             authorization-grant-type = "authorization_code";
+  #             redirect-uri = "{baseUrl}/login/oauth2/code/{registrationId}";
+  #             client-authentication-method = "client_secret_post";
+  #           };
+  #           provider.pocket-id = {
+  #             user-name-attribute = "email";
+  #             issuer-uri = "https://id.arnaut.me";
+  #           };
+  #         };
+  #       };
+  #     };
+  #     sops = {
+  #       secrets = {
+  #         "services/komga/client-id".owner = config.services.komga.user;
+  #         "services/komga/client-secret".owner = config.services.komga.user;
+  #       };
+  #       templates."komga.env" = {
+  #         content = ''
+  #           KOMGA_OAUTH_CLIENT_ID=${config.sops.placeholder."services/komga/client-id"}
+  #           KOMGA_OAUTH_CLIENT_SECRET=${config.sops.placeholder."services/komga/client-secret"}
+  #         '';
+  #         owner = config.services.komga.user;
+  #       };
+  #     };
+  #     systemd.services.komga.serviceConfig.EnvironmentFile = config.sops.templates."komga.env".path;
+  #   })
+  #   (lib.mkIf (config.services.komga.enable && config.services.cloudflared.enable) {
+  #     services.cloudflared.tunnels."${config.networking.hostName}".ingress = [
+  #       {
+  #         hostname = "library.${config.services.cloudflared.defaultDomain}";
+  #         # path = "/prowlarr.*";
+  #         service = "http://localhost:${builtins.toString config.services.komga.settings.server.port}";
+  #       }
+  #     ];
+  #   })
+  # ];
 }
