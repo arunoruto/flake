d0ce4de7f5639bc0e17be2459fff15a6b1b78c4e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-12-15 22:10:35 +0100

    refactor(ls): move assertions and warnings to config
    This change refactors the `ls` home-manager module to improve its
    structure and adhere more closely to idiomatic Nix patterns.
    
    The assertions and warnings are now grouped under the `config`
    attribute. The conditional logic for warnings is simplified by using
    `lib.optional`.

--- modules/home-manager/background/core-utils/ls.nix
+++ modules/home-manager/background/core-utils/ls.nix
@@ -5,57 +5,53 @@
   ...
 }:
 {
-  assertions = [
-    {
-      assertion = (with config.programs; lsd.enable && eza.enable);
-      message = ''
-        Both lsd and eza are enabled.
-        It is recommended to use only one of these 'ls' replacements to avoid confusion.
-      '';
-    }
-  ];
-  warnings =
-    if (with config.programs; !(lsd.enable || eza.enable)) then
-      [
-        ''
-          Warning: Neither lsd nor eza is enabled.
-          It is recommended to enable one of these 'ls' replacements for an improved terminal experience.
-        ''
-      ]
-    else
-      [ ];
+  config = {
+    assertions = [
+      {
+        assertion = (config.programs.lsd.enable && config.programs.eza.enable);
+        message = ''
+          Both lsd and eza are enabled.
+          It is recommended to use only one of these 'ls' replacements to avoid confusion.
+        '';
+      }
+    ];
+    warnings = lib.optional (!(config.programs.lsd.enable || config.programs.eza.enable)) ''
+      Warning: Neither lsd nor eza is enabled.
+      It is recommended to enable one of these 'ls' replacements for an improved terminal experience.
+    '';
 
-  programs = {
-    lsd = {
-      # enableAliases = true;
-      # enableBashIntegration = config.programs.bash.enable;
-      # enableFishIntegration = config.programs.fish.enable;
-      # enableZshIntegration = config.programs.zsh.enable;
-      settings = {
-        ignore-globs = [
-          ".DS_Store"
-        ];
-        blocks = [
-          "permission"
-          "user"
-          "group"
-          "size"
-          "date"
-          "git"
-          "name"
-        ];
-        date = "+%F %T";
-        sorting = {
-          dir-grouping = "first";
+    programs = {
+      lsd = {
+        # enableAliases = true;
+        # enableBashIntegration = config.programs.bash.enable;
+        # enableFishIntegration = config.programs.fish.enable;
+        # enableZshIntegration = config.programs.zsh.enable;
+        settings = {
+          ignore-globs = [
+            ".DS_Store"
+          ];
+          blocks = [
+            "permission"
+            "user"
+            "group"
+            "size"
+            "date"
+            "git"
+            "name"
+          ];
+          date = "+%F %T";
+          sorting = {
+            dir-grouping = "first";
+          };
         };
       };
-    };
 
-    eza = {
-      color = "always";
-      git = true;
-      icons = true;
-      extraOptions = [ "--group-directories-first" ];
+      eza = {
+        color = "always";
+        git = true;
+        icons = true;
+        extraOptions = [ "--group-directories-first" ];
+      };
     };
   };
 }
