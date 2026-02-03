50acce8fc5da8baa6b9b49777a8a53e72ab5a09f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-02-03 02:08:51 +0100

    build(opencode): bump version to 1.1.48

--- packages/top-level/opencode/package.nix
+++ packages/top-level/opencode/package.nix
@@ -14,12 +14,12 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.1.35";
+  version = "1.1.48";
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-z8RnhFbdtZPb05l1gdgKXCOBDgRNZyppgzrFRNq4wmg=";
+    hash = "sha256-zKkeJSsxEuhucQkWBHxLR7tCTu86q2p6neRST2g/1hA=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -66,7 +66,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-IlRtdiY6kpscHcFNcmI5dW3LbRh9e+3PpafNPM7wTIA=";
+    outputHash = "sha256-bSNUuMTajtj4uuyxU3cSS6xaSihazjI2fTo62VU8MZk=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
