0f402906ef75db761bc12c1553d8681eec5b3c03
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-02-04 01:50:18 +0100

    refactor(darwin-defaults): move default values to default.nix

--- modules/darwin/system/default.nix
+++ modules/darwin/system/default.nix
@@ -1,12 +1,43 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   imports = [
-    ./defaults.nix
     ./nix.nix
   ];
+
+  system.defaults = {
+    dock = {
+      autohide = lib.mkDefault true;
+      autohide-delay = 0.25;
+    };
+
+    # Finder settings
+    finder = {
+      AppleShowAllExtensions = lib.mkDefault true;
+      FXEnableExtensionChangeWarning = lib.mkDefault false;
+    };
+
+    # Trackpad settings
+    trackpad = {
+      Clicking = lib.mkDefault true;
+      TrackpadRightClick = lib.mkDefault true;
+    };
+  };
+
+  # Keyboard settings
+  system.keyboard = {
+    enableKeyMapping = lib.mkDefault true;
+    remapCapsLockToEscape = lib.mkDefault true;
+    swapLeftCtrlAndFn = lib.mkDefault true;
+    userKeyMapping = [
+      (lib.mkIf config.system.keyboard.remapCapsLockToEscape {
+        HIDKeyboardModifierMappingSrc = 30064771113;
+        HIDKeyboardModifierMappingDst = 30064771129;
+      })
+    ];
+  };
+
 }
