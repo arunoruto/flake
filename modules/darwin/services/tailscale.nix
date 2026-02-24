4865b6afe425f44fe4be77efe1970893d0e7207a
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-02-24 06:13:00 +0100

    feat: alias tailscale command to mac store app if service is not present!

--- modules/darwin/services/tailscale.nix
+++ modules/darwin/services/tailscale.nix
@@ -1,9 +1,17 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   config = {
     services.tailscale = {
       # package = pkgs.unstable.tailscale;
       package = if (pkgs ? "unstable") then pkgs.unstable.tailscale else pkgs.tailscale;
+    };
+    environment.shellAliases = lib.mkIf (!config.services.tailscale.enable) {
+      tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
     };
   };
 }
