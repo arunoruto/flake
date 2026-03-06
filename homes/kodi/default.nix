df94815c69350c395c4694c9cc05cfb21d8f2ea7
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-03-07 00:17:19 +0100

    feat: disable zsh and nushell in kodi and mirza configs
    [body]
    - Disabled nushell and zsh in Kodi default config
    - Added Darwin-specific zsh enablement for Mirza config
    - Improved config structure in Mirza to include pkgs

--- homes/kodi/default.nix
+++ homes/kodi/default.nix
@@ -4,8 +4,8 @@
     shell.main = "fish";
     programs = lib.optionalAttrs (!config.hosts.tinypc.enable) {
       fish.enable = true;
-      nushell.enable = true;
-      zsh.enable = true;
+      nushell.enable = false;
+      zsh.enable = false;
     };
   };
 }
--- homes/mirza/default.nix
+++ homes/mirza/default.nix
@@ -1,11 +1,16 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   config = {
     shell.main = "fish";
     programs = lib.optionalAttrs (!config.hosts.tinypc.enable) {
       fish.enable = true;
       nushell.enable = true;
-      zsh.enable = true;
+      zsh.enable = pkgs.stdenv.hostPlatform.isDarwin;
     };
   };
 }
