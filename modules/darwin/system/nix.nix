809c3024b15a785aa43c213feee70e04d4ffa955
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-03-08 02:45:35 +0100

    feat(darwin): enable flake-based Nix configuration defaults
    ```

--- modules/darwin/system/nix.nix
+++ modules/darwin/system/nix.nix
@@ -1,26 +1,31 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
+let
+  settings = {
+    warn-dirty = lib.mkDefault false;
+    accept-flake-config = lib.mkDefault true;
+    trusted-users = lib.mkDefault [
+      "root"
+      "@admin"
+    ];
+    extra-experimental-features = lib.mkDefault [
+      "nix-command"
+      "flakes"
+      "pipe-operators"
+    ];
+  };
+in
 {
   config = {
     nix = {
       enable = lib.mkDefault false;
-      settings = {
-        warn-dirty = lib.mkDefault false;
-        accept-flake-config = lib.mkDefault true;
-        trusted-users = lib.mkDefault [
-          "root"
-          "@wheel"
-        ];
-        extra-experimental-features = lib.mkDefault [
-          "nix-command"
-          "flakes"
-          "pipe-operators"
-        ];
-      };
+      inherit settings;
     };
+  }
+  // lib.mkIf (config ? determinateNix) {
+    determinateNix.customSettings = settings;
   };
 }
