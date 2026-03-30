c603e42593092fbc180ee6c353b6ca38181d03b1
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-03-30 08:58:09 +0200

    chore: disable not needed cli features

--- modules/home-manager/background/cli/default.nix
+++ modules/home-manager/background/cli/default.nix
@@ -25,7 +25,7 @@
       helix.enable = lib.mkDefault true;
 
       atuin.enable = lib.mkDefault true;
-      serpl.enable = lib.mkDefault true;
+      serpl.enable = lib.mkDefault false;
       yazi.enable = lib.mkDefault true;
       zellij.enable = lib.mkDefault false;
     };
