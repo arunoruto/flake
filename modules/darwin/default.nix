5404aaf91d7a5ff99567a7bb98f9306d6fbb9887
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-05-09 03:29:01 +0200

    feat(darwin): configure shell ulimit and add tree

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -13,4 +13,14 @@
     ./users
     ../../homes/nixos.nix
   ];
+
+  environment = {
+    shellInit = ''
+      # Increase the limit of open files for all interactive shells
+      ulimit -n 4096 2>/dev/null
+    '';
+    systemPackages = with pkgs; [
+      tree
+    ];
+  };
 }
