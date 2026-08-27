897407d1e39255ab7cad6aa70dfcd5641942d93a
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-08-27 03:25:06 +0200

    fix: install a stable version of libreoffice
    we don't need the bleeding edge version of libre office :)

--- modules/home-manager/foreground/documents/libreoffice.nix
+++ modules/home-manager/foreground/documents/libreoffice.nix
@@ -8,18 +8,15 @@
   options.programs.libreoffice.enable = lib.mkEnableOption "Enable the document suite LibreOffice";
 
   config = lib.mkIf config.programs.libreoffice.enable {
-    home.packages =
-      (with pkgs.unstable; [
-        libreoffice-fresh
-      ])
-      ++ (with pkgs; [
-        # libreoffice-qt
-        hunspell
-        hunspellDicts.en-us
-        hunspellDicts.en-gb-ise
-        hunspellDicts.de-de
-        hyphenDicts.de-de
-        hyphenDicts.en-us
-      ]);
+    home.packages = with pkgs; [
+      libreoffice-fresh
+      # libreoffice-qt
+      hunspell
+      hunspellDicts.en-us
+      hunspellDicts.en-gb-ise
+      hunspellDicts.de-de
+      hyphenDicts.de-de
+      hyphenDicts.en-us
+    ];
   };
 }
